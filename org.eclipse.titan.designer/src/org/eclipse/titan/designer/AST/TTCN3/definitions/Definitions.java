298667734bcc5f461b3e63149cb3884dad320674
balaskoa <jeno.balasko@ericsson.com>
2017-02-14 11:44:45 +0100

    Incremental parsing: marker cleanup for just commented lines near to eof (Bug 508116)
    Signed-off-by: balaskoa <jeno.balasko@ericsson.com>

--- org.eclipse.titan.designer/src/org/eclipse/titan/designer/AST/TTCN3/definitions/Definitions.java
+++ org.eclipse.titan.designer/src/org/eclipse/titan/designer/AST/TTCN3/definitions/Definitions.java
@@ -383,19 +383,32 @@ public final class Definitions extends Assignments implements ILocateableNode {
 		
 		//markers on imports cannot be removed, they are already refreshed
 		
+		//These offsets will be used to remove last comments within Definitions
+		int maxEndOffset = 0;
+		int lastEndOffset = 0;
+		
 		for(Definition definition : definitions){
-			if(definition.getLastTimeChecked() == null || definition.getLastTimeChecked().isLess(timestamp)){
+			if (definition.getLastTimeChecked() == null || definition.getLastTimeChecked().isLess(timestamp) ){
 				MarkerHandler.markAllSemanticMarkersForRemoval(definition.getCommentLocation()); 
 				MarkerHandler.markAllSemanticMarkersForRemoval(definition); //doubleDefinition report will be deleted!
+				lastEndOffset = definition.getLocation().getEndOffset();
+				if (lastEndOffset > maxEndOffset) { 
+					maxEndOffset = lastEndOffset;
+				}
 			}
 		}
-		
+
 		for(Group group: groups){
 			group.markMarkersForRemoval(timestamp);
 		}
-		
-		//TODO: remove markers on commented lines
-		
+
+		//remove markers on the final commented lines (for incremental parsing)
+		final int defsEndOffset = getLocation().getEndOffset();
+		if (maxEndOffset < defsEndOffset) {
+			final Location loc = new Location(getLocation().getFile(), 0, maxEndOffset, defsEndOffset);
+			MarkerHandler.markAllSemanticMarkersForRemoval(loc);
+		}
+				
 		checkUniqueness(timestamp);
 		checkGroups(timestamp);
 		for( Definition definition: definitions){
