edc1a3aab2bb18adb45566a85f9fc2c278fcd692
earplov <arpad.lovassy@ericsson.com>
2017-04-07 15:13:08 +0200

    java compiler: initial draft of logger with empty functions
    Signed-off-by: earplov <arpad.lovassy@ericsson.com>

---
+++ org.eclipse.titan.runtime/src/org/eclipse/titan/runtime/core/TtcnLogger.java
@@ -0,0 +1,49 @@
+package org.eclipse.titan.runtime.core;
+
+/**
+ * originally TTCN_Logger
+ * @author Arpad Lovassy
+ */
+public class TtcnLogger {
+
+	public static void log_event( String format, Object... args ) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public static void log_event_str( String string ) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public static void log_char( char c ) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public static void log_event_uninitialized() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public static void log_hex( byte aHexDigit ) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public static void log_event_unbound() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public static void log_octet( char aOctet ) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public static void log_bit( byte aBit ) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
