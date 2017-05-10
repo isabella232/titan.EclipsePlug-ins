01975f8d3421fec144b70cce15747837e329ffe8
kristof <Kristof.Szabados@ericsson.com>
2017-05-10 11:54:46 +0200

    lets push the boundary of tryAltsteps into the core
    Signed-off-by: kristof <Kristof.Szabados@ericsson.com>

---
+++ org.eclipse.titan.runtime/src/org/eclipse/titan/runtime/core/TTCN_Default.java
@@ -0,0 +1,21 @@
+package org.eclipse.titan.runtime.core;
+
+/**
+ * This class is used to provide default handling in the runtime.
+ *
+ * @author Kristof Szabados
+ * */
+public class TTCN_Default {
+
+	private TTCN_Default() {
+		//intentionally empty to disable instantiation
+	}
+
+	public static TitanAlt_Status tryAltsteps() {
+		TitanAlt_Status returnValue = TitanAlt_Status.ALT_NO;
+
+		// FIXME implement the rest of the functionality
+
+		return returnValue;
+	}
+}
