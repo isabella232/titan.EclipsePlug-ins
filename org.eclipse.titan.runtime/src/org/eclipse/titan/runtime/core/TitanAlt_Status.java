6d2b831266e829d4818ea924a00672de8c8f208e
kristof <Kristof.Szabados@ericsson.com>
2017-05-15 15:29:44 +0200

    added some comments
    Signed-off-by: kristof <Kristof.Szabados@ericsson.com>

--- org.eclipse.titan.runtime/src/org/eclipse/titan/runtime/core/TitanAlt_Status.java
+++ org.eclipse.titan.runtime/src/org/eclipse/titan/runtime/core/TitanAlt_Status.java
@@ -9,5 +9,19 @@ package org.eclipse.titan.runtime.core;
  */
 public enum TitanAlt_Status {
 	// TODO we might not need the ALT prefix in Java
-	ALT_UNCHECKED, ALT_YES, ALT_MAYBE, ALT_NO, ALT_REPEAT, ALT_BREAK;
+	// TODO check if we use unchecked and maybe correctly
+
+	// the branch status was not yet checked
+	ALT_UNCHECKED,
+	// the branch was selected/guard returned true
+	ALT_YES,
+	// the branch is not yet selectable, but could be
+	// for example a timer has not yet timedout
+	ALT_MAYBE,
+	// the branch can not be choosen
+	ALT_NO,
+	// a repeat statement was encountered, the guards have to be evaluated again
+	ALT_REPEAT,
+	// a break was encountered, the alt/altstep must be exited
+	ALT_BREAK;
 }
