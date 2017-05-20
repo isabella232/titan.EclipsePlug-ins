6bc634d12eb1e87565318460de32f1c53e8d47e0
Farkas Izabella Ingrid <farkas94ingi@gmail.com>
2017-05-20 19:58:24 +0200

    Organize imports refactoring
    Signed-off-by: Farkas Izabella Ingrid <farkas94ingi@gmail.com>

---
+++ org.eclipse.titanium.refactoring/src/org/eclipse/titanium/refactoring/organize/OrganizeImportsWizard.java
@@ -0,0 +1,41 @@
+/******************************************************************************
+ * Copyright (c) 2000-2017 Ericsson Telecom AB
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ ******************************************************************************/
+package org.eclipse.titanium.refactoring.organize;
+
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IConfigurationElement;
+import org.eclipse.core.runtime.IExecutableExtension;
+import org.eclipse.ltk.core.refactoring.Refactoring;
+import org.eclipse.ltk.ui.refactoring.RefactoringWizard;
+
+/**
+ * Wizard for the 'Organize imports modifires' operation.
+ * 
+ * @author Farkas Izabella Ingrid
+ */
+
+public class OrganizeImportsWizard  extends RefactoringWizard implements IExecutableExtension {
+
+	private static final String WIZ_WINDOWTITLE = "Organize imports modifiers";
+
+	public OrganizeImportsWizard(final Refactoring refactoring) {
+		super(refactoring, DIALOG_BASED_USER_INTERFACE);
+	}
+
+	@Override
+	protected void addUserInputPages() {
+		setDefaultPageTitle(WIZ_WINDOWTITLE);
+	}
+
+	@Override
+	public void setInitializationData(IConfigurationElement config, String propertyName, Object data)
+			throws CoreException {
+		// TODO Auto-generated method stub
+	}
+	
+}
