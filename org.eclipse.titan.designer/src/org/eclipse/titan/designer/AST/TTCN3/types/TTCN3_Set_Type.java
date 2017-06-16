f8084e92f738c27f5da0d3f5363f8143486855db
Kristof Szabados <Kristof.Szabados@ericsson.com>
2017-06-16 11:59:38 +0200

    the template variant is not yet generated
    Signed-off-by: Kristof Szabados <Kristof.Szabados@ericsson.com>

--- org.eclipse.titan.designer/src/org/eclipse/titan/designer/AST/TTCN3/types/TTCN3_Set_Type.java
+++ org.eclipse.titan.designer/src/org/eclipse/titan/designer/AST/TTCN3/types/TTCN3_Set_Type.java
@@ -690,6 +690,10 @@ public final class TTCN3_Set_Type extends TTCN3_Set_Seq_Choice_BaseType {
 		}
 
 		RecordSetCodeGenerator.generateValueClass(aData, source, className, classReadableName, namesList, hasOptional, true);
+
+		source.append( "//TODO: generating template set in" );
+		source.append( getClass().getSimpleName() );
+		source.append( ".generateCode() is not implemented!\n" );
 	}
 
 	@Override
