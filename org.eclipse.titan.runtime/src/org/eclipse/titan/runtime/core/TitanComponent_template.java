665157fb300a94045bf8aa47d7ea6c22beb9a7a5
Kristof Szabados <Kristof.Szabados@ericsson.com>
2017-06-19 13:17:00 +0200

    minimal implementations need for the port.receive
    Signed-off-by: Kristof Szabados <Kristof.Szabados@ericsson.com>

--- org.eclipse.titan.runtime/src/org/eclipse/titan/runtime/core/TitanComponent_template.java
+++ org.eclipse.titan.runtime/src/org/eclipse/titan/runtime/core/TitanComponent_template.java
@@ -5,5 +5,31 @@ package org.eclipse.titan.runtime.core;
  * @author Kristof Szabados
  */
 public class TitanComponent_template extends Base_Template {
+	public static final TitanComponent_template any_compref = new TitanComponent_template(template_sel.ANY_VALUE);
 
+	public TitanComponent_template() {
+		//intentionally empty
+	}
+
+	public TitanComponent_template(final template_sel otherValue) {
+		super(otherValue);
+		checkSingleSelection(otherValue);
+	}
+
+	// originally match
+	public TitanBoolean match(final TitanComponent otherValue) {
+		return match(otherValue, false);
+	}
+
+	// originally match
+	public TitanBoolean match(final TitanComponent otherValue, final boolean legacy) {
+		//FIXME not yet implemented
+		return new TitanBoolean(false);
+	}
+
+	// originally match
+	public TitanBoolean match(final int otherValue, final boolean legacy) {
+		//FIXME not yet implemented
+		return new TitanBoolean(false);
+	}
 }
