c1f82ef8bf520a51b6da30e200f37c2afd1d55ef
Kristof Szabados <Kristof.Szabados@ericsson.com>
2017-06-21 09:39:50 +0200

    added some base functions
    Signed-off-by: Kristof Szabados <Kristof.Szabados@ericsson.com>

--- org.eclipse.titan.runtime/src/org/eclipse/titan/runtime/core/TitanInteger_template.java
+++ org.eclipse.titan.runtime/src/org/eclipse/titan/runtime/core/TitanInteger_template.java
@@ -324,4 +324,39 @@ public class TitanInteger_template extends Base_Template {
 	public void setMaxExclusive(final boolean maxExclusive) {
 		max_is_exclusive = maxExclusive;
 	}
+
+	//originally is_present
+	public boolean isPresent(final boolean legacy) {
+		if (templateSelection == template_sel.UNINITIALIZED_TEMPLATE) {
+			return false;
+		}
+
+		return !match_omit(legacy);
+	}
+
+	public boolean match_omit(final boolean legacy) {
+		if (is_ifPresent) {
+			return true;
+		}
+
+		switch(templateSelection) {
+		case OMIT_VALUE:
+		case ANY_OR_OMIT:
+			return true;
+		case VALUE_LIST:
+		case COMPLEMENTED_LIST:
+			if (legacy) {
+				// legacy behavior: 'omit' can appear in the value/complement list
+				for (int i = 0; i < value_list.size(); i++) {
+					if (value_list.get(i).match_omit(legacy)) {
+						return templateSelection == template_sel.VALUE_LIST;
+					}
+				}
+				return templateSelection == template_sel.COMPLEMENTED_LIST;
+			}
+			return false;
+		default:
+			return false;
+		}
+	}
 }
