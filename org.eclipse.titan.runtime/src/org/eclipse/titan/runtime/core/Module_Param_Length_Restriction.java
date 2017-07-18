144edf18896eefce075a7bd6276cb1fe1623ca95
zlovarp <arpad.lovassy@semcon.com>
2017-07-18 11:42:59 +0200

    java compiler: Record_Of_Template base class added
    Signed-off-by: zlovarp <arpad.lovassy@semcon.com>

---
+++ org.eclipse.titan.runtime/src/org/eclipse/titan/runtime/core/Module_Param_Length_Restriction.java
@@ -0,0 +1,60 @@
+/******************************************************************************
+ * Copyright (c) 2000-2017 Ericsson Telecom AB
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ ******************************************************************************/
+package org.eclipse.titan.runtime.core;
+
+/**
+ * Module_Param_Length_Restriction in titan.core
+ * 
+ * @author Arpad Lovassy
+ */
+public class Module_Param_Length_Restriction {
+	private int min;
+	private boolean has_max;
+	private int max;
+
+	public Module_Param_Length_Restriction() {
+		min = 0;
+		has_max = false;
+		max = 0;
+	}
+
+	public void set_single( int p_single ) {
+		has_max = true;
+		min = max = p_single;
+	}
+
+	public void set_min( int p_min ) {
+		min=p_min;
+	}
+
+	public void set_max( int p_max ) {
+		has_max = true;
+		max = p_max;
+	}
+
+	public int get_min() {
+		return min;
+	}
+
+	public boolean get_has_max() {
+		return has_max;
+	}
+
+	public int get_max() {
+		return max;
+	}
+
+	public boolean is_single() {
+		return has_max && min==max;
+	}
+
+	public void log() {
+		//TODO: implement
+	}
+
+}
---
+++ org.eclipse.titan.runtime/src/org/eclipse/titan/runtime/core/Record_Of_Template.java
@@ -0,0 +1,172 @@
+/******************************************************************************
+ * Copyright (c) 2000-2017 Ericsson Telecom AB
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ ******************************************************************************/
+package org.eclipse.titan.runtime.core;
+
+import java.text.MessageFormat;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Record_Of_Template in titan.core
+ * 
+ * @author Arpad Lovassy
+ */
+public abstract class Record_Of_Template extends Restricted_Length_Template {
+
+	//originally single_value/value_elements
+	List<Base_Template> value_elements;
+
+	//originally value_list/list_value
+	List<Record_Of_Template> list_value;
+
+	/**
+	 * permutation interval 
+	 */
+	class Pair_of_elements {
+		//beginning and ending index
+		private int start_index;
+		private int end_index;
+
+		public Pair_of_elements( int start_index, int end_index ) {
+			this.start_index = start_index;
+			this.end_index = end_index;
+		}
+	};
+
+	List<Pair_of_elements> permutation_intervals;
+
+	Record_Of_Template() {
+		permutation_intervals = null;
+	}
+
+	Record_Of_Template(template_sel other_value) {
+		super(other_value);
+		permutation_intervals = null;
+	}
+
+	void clean_up_intervals() {
+		permutation_intervals = null;
+	}
+
+	void set_selection(template_sel other_value) {
+		super.set_selection(other_value);
+		clean_up_intervals();
+	}
+
+	void set_selection(final Record_Of_Template other_value) {
+		super.set_selection(other_value);
+		clean_up_intervals();
+		if(other_value.templateSelection == template_sel.SPECIFIC_VALUE) {
+			permutation_intervals = copyPermutations( other_value.permutation_intervals );
+		}
+	}
+
+	public final List<Pair_of_elements> copyPermutations( final List<Pair_of_elements> srcList ) {
+		if ( srcList == null ) {
+			return null;
+		}
+
+		final List<Pair_of_elements> newList = new ArrayList<Pair_of_elements>( srcList.size() );
+		for (Pair_of_elements srcElem : srcList) {
+			Pair_of_elements newElem = new Pair_of_elements( srcElem.start_index, srcElem.start_index );
+			newList.add( newElem );
+		}
+		return newList;
+	}
+
+	//TODO: implement void encode_text_permutation(Text_Buf text_buf)
+	//TODO: implement void decode_text_permutation(Text_Buf text_buf)
+
+	void add_permutation(int start_index, int end_index) {
+		if(start_index > end_index) {
+			throw new TtcnError( MessageFormat.format( "wrong permutation interval settings start ({0})can not be greater than end ({1})",start_index, end_index ) );
+		}
+
+		final int number_of_permutations = get_number_of_permutations();
+		if(number_of_permutations > 0 &&
+				permutation_intervals.get( number_of_permutations - 1 ).end_index >= start_index) {
+			//TODO: fix {0}th (also in titan.core), it can be 1st, 2nd, 3rd, but 11th, 12th, 13th
+			//throw new TtcnError( MessageFormat.format( "the {0}{1} permutation overlaps the previous one", number_of_permutations, getOrdinalIndicator(number_of_permutations) ) );
+			throw new TtcnError( MessageFormat.format( "the {0}th permutation overlaps the previous one", number_of_permutations ) );
+		}
+
+		final Pair_of_elements newElem = new Pair_of_elements( start_index, end_index );
+		permutation_intervals.add( newElem );
+	}
+
+	//TODO: move it to a utility class
+	/**
+	 * Calculates the ordinal indicator for an integer number, like 5th, it can be 1st, 2nd, 3rd, but 11th, 12th, 13th
+	 * @param n integer number
+	 * @return st, nd, rd or th
+	 */
+	private static String getOrdinalIndicator( int n ) {
+		if ( 11 <= n % 100 && n % 100 <= 13 ) {
+			//exception case
+			return "th";
+		}
+		switch ( n % 10 ) {
+		case 1:
+			return "st";
+		case 2:
+			return "nd";
+		case 3:
+			return "rd";
+		default:
+			return "th";
+		}
+	}
+
+	public int get_number_of_permutations() {
+		return permutation_intervals != null ? permutation_intervals.size() : 0;
+	}
+
+	public int get_permutation_start(int index_value) {
+		if(index_value >= get_number_of_permutations()) {
+			throw new TtcnError( MessageFormat.format( "Index overflow ({0})", index_value ) );
+		}
+
+		return permutation_intervals.get(index_value).start_index;
+	}
+
+	public int get_permutation_end(int index_value) {
+		if(index_value >= get_number_of_permutations()) {
+			throw new TtcnError( MessageFormat.format( "Index overflow ({0})", index_value ) );
+		}
+
+		return permutation_intervals.get(index_value).end_index;
+	}
+
+	public int get_permutation_size(int index_value) {
+		if(index_value >= get_number_of_permutations()) {
+			throw new TtcnError( MessageFormat.format( "Index overflow ({0})", index_value ) );
+		}
+
+		return permutation_intervals.get(index_value).end_index - permutation_intervals.get(index_value).start_index + 1;
+	}
+
+	boolean permutation_starts_at(int index_value) {
+		final int number_of_permutations = get_number_of_permutations();
+		for(int i = 0; i < number_of_permutations; i++) {
+			if(permutation_intervals.get( i ).start_index == index_value)
+				return true;
+		}
+
+		return false;
+	}
+
+	boolean permutation_ends_at(int index_value) {
+		final int number_of_permutations = get_number_of_permutations();
+		for(int i = 0; i < number_of_permutations; i++) {
+			if(permutation_intervals.get( i ).end_index == index_value)
+				return true;
+		}
+
+		return false;
+	}
+}
