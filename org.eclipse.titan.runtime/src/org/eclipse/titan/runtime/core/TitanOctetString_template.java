1fea07da049118ad67ff0297b2be2320b5bec7e6
Fakas Izabella Ingrid <farkas94ingi@gmail.com>
2017-07-19 16:57:05 +0200

    implemented functions for TitanOctetString_template.
    Signed-off-by: Fakas Izabella Ingrid <farkas94ingi@gmail.com>

--- org.eclipse.titan.runtime/src/org/eclipse/titan/runtime/core/TitanOctetString_template.java
+++ org.eclipse.titan.runtime/src/org/eclipse/titan/runtime/core/TitanOctetString_template.java
@@ -15,7 +15,7 @@ import java.util.List;
  *
  * @author Arpad Lovassy
  */
-public class TitanOctetString_template extends Base_Template {
+public class TitanOctetString_template extends Restricted_Length_Template {
 
 	private TitanOctetString single_value;
 
@@ -51,6 +51,13 @@ public class TitanOctetString_template extends Base_Template {
 		otherValue.mustBound("Creating a template from an unbound octetstring value.");
 
 		single_value = new TitanOctetString(otherValue);
+	}
+
+	public TitanOctetString_template (final TitanOctetString_Element otherValue) {
+		super(template_sel.SPECIFIC_VALUE);
+		otherValue.mustBound("Creating a template from an unbound octetstring value.");
+
+		single_value = new TitanOctetString(otherValue.get_nibble());
 	}
 
 	public TitanOctetString_template (final TitanOctetString_template otherValue) {
@@ -98,6 +105,16 @@ public class TitanOctetString_template extends Base_Template {
 		cleanUp();
 		setSelection(template_sel.SPECIFIC_VALUE);
 		single_value = new TitanOctetString(otherValue);
+
+		return this;
+	}
+
+	public TitanOctetString_template assign( final TitanOctetString_Element otherValue ) {
+		otherValue.mustBound("Assignment of an unbound octetstring value to a template.");
+
+		cleanUp();
+		setSelection(template_sel.SPECIFIC_VALUE);
+		single_value = new TitanOctetString(otherValue.get_nibble());
 
 		return this;
 	}
@@ -218,10 +235,131 @@ public class TitanOctetString_template extends Base_Template {
 			} else if ( digit == 257 ) {
 				sb.append( '*' );
 			} else {
-				sb.append( TitanHexString.HEX_DIGITS.charAt( digit >> 8 ) );
-				sb.append( TitanHexString.HEX_DIGITS.charAt( digit & 0xFF ) );
+				sb.append( TitanHexString.HEX_DIGITS.charAt( digit >> 4 ) );
+				sb.append( TitanHexString.HEX_DIGITS.charAt( digit % 16 ) );
 			}
 		}
 		return sb.toString();
 	}
+
+	public TitanOctetString valueOf() {
+		if (templateSelection != template_sel.SPECIFIC_VALUE || is_ifPresent)
+			throw new TtcnError("Performing a valueof or send operation on a non-specific octetstring template.");
+		return single_value;
+	}
+
+	public TitanInteger lengthOf() {
+		int min_length;
+		boolean has_any_or_none;
+		if (is_ifPresent)
+			throw new TtcnError("Performing lengthOf() operation on a octetstring template which has an ifpresent attribute.");
+
+		switch (templateSelection)
+		{
+		case SPECIFIC_VALUE:
+			min_length = single_value.lengthOf().getInt();
+			has_any_or_none = false;
+			break;
+		case OMIT_VALUE:
+			throw new TtcnError("Performing lengthof() operation on an octetstring template containing omit value.");
+		case ANY_VALUE:
+		case ANY_OR_OMIT:
+			min_length = 0;
+			has_any_or_none = true; // max. length is infinity
+			break;
+		case VALUE_LIST:
+		{
+			// error if any element does not have length or the lengths differ
+			if (value_list.size() < 1)
+				throw new TtcnError("Internal error: Performing lengthOf() operation on an octetstring template "
+						+"containing an empty list.");
+			int item_length = value_list.get(0).lengthOf().getInt();
+			for (int i = 1; i < value_list.size(); i++) {
+				if (value_list.get(i).lengthOf().getInt() != item_length)
+					throw new TtcnError("Performing lengthof() operation on an octetstring template "
+							+"containing a value list with different lengths.");
+			}
+			min_length = item_length;
+			has_any_or_none = false;
+			break;
+		}
+		case COMPLEMENTED_LIST:
+			throw new TtcnError("Performing lengthof() operation on an octetstring template containing complemented list.");
+		case STRING_PATTERN:
+			//TODO: implement
+			min_length = 0;
+			has_any_or_none = false; // TRUE if * chars in the pattern
+			/*
+			for (int i = 0; i < pattern_value->n_elements; i++)
+			{
+				if (pattern_value->elements_ptr[i] < 257) min_length++;
+				else has_any_or_none = TRUE;   // case of * character
+			}*/
+			break;
+		default:
+			throw new TtcnError("Performing lengthof() operation on an uninitialized/unsupported octetstring template.");
+		}
+		
+		return new TitanInteger(check_section_is_single(min_length, has_any_or_none, "length", "an", "octetstring template"));
+	}
+
+	public void setType(template_sel template_type) {
+		setType(template_type,0);
+	}
+
+	public void setType(template_sel template_type, int list_length) {
+		if (template_type != template_sel.VALUE_LIST && template_type != template_sel.COMPLEMENTED_LIST &&
+				template_type != template_sel.DECODE_MATCH)
+			throw new TtcnError("Setting an invalid type for an octetstring template.");
+		cleanUp();
+		setSelection(template_type);
+		if (template_type != template_sel.DECODE_MATCH) {
+			value_list = new ArrayList<TitanOctetString_template>(list_length);
+		}
+	}
+
+	public TitanOctetString_template listItem(int listIndex) {
+		if (templateSelection != template_sel.VALUE_LIST &&
+				templateSelection != template_sel.COMPLEMENTED_LIST)
+			throw new TtcnError("Accessing a list element of a non-list octetstring template.");
+		if (listIndex >= value_list.size())
+			throw new TtcnError("Index overflow in an octetstring value list template.");
+		return value_list.get(listIndex);
+	}
+
+	public boolean isPresent(){
+		return isPresent(false);
+	}
+
+	public boolean isPresent(boolean legacy) {
+		if (templateSelection==template_sel.UNINITIALIZED_TEMPLATE) return false;
+
+		return !matchOmit(legacy).getValue();
+	}
+
+	public TitanBoolean matchOmit() {
+		return matchOmit(false);
+	}
+
+	public TitanBoolean matchOmit(boolean legacy) {
+		if (is_ifPresent) return new TitanBoolean(true);
+		switch (templateSelection) {
+		case OMIT_VALUE:
+		case ANY_OR_OMIT:
+			return new TitanBoolean(true);
+		case VALUE_LIST:
+		case COMPLEMENTED_LIST:
+			if (legacy) {
+				// legacy behavior: 'omit' can appear in the value/complement list
+				for (int i=0; i<value_list.size(); i++)
+					if (value_list.get(i).matchOmit().getValue())
+						return new TitanBoolean(templateSelection==template_sel.VALUE_LIST);
+				return new TitanBoolean(templateSelection==template_sel.COMPLEMENTED_LIST);
+			}
+			// else fall through
+		default:
+			return new TitanBoolean(false);
+		}
+	}
+
 }
