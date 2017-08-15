51df5849e4026e2fc476e0fc46fb5ea0ae645618
kristof <Kristof.Szabados@ericsson.com>
2017-08-15 11:47:38 +0200

    isPresent and match_omit without parameters
    Signed-off-by: kristof <Kristof.Szabados@ericsson.com>

--- org.eclipse.titan.designer/src/org/eclipse/titan/designer/AST/TTCN3/types/UnionGenerator.java
+++ org.eclipse.titan.designer/src/org/eclipse/titan/designer/AST/TTCN3/types/UnionGenerator.java
@@ -767,6 +767,10 @@ public class UnionGenerator {
 	 * @param source: where the source code is to be generated.
 	 * */
 	private static void generateTemplateIsPresent(final StringBuilder source) {
+		source.append("public boolean isPresent() {\n");
+		source.append("return isPresent(false);\n");
+		source.append("}\n\n");
+
 		source.append("public boolean isPresent(final boolean legacy) {\n");
 		source.append("if (templateSelection == template_sel.UNINITIALIZED_TEMPLATE) {\n");
 		source.append("return false;\n");
@@ -781,6 +785,10 @@ public class UnionGenerator {
 	 * @param source: where the source code is to be generated.
 	 * */
 	private static void generateTemplateMatchOmit(final StringBuilder source) {
+		source.append("public boolean match_omit() {\n");
+		source.append("return match_omit(false);\n");
+		source.append("}\n\n");
+
 		source.append("public boolean match_omit(final boolean legacy) {\n");
 		source.append("if (is_ifPresent) {\n");
 		source.append("return true;\n");
