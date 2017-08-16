95638ca487c520215bf57e86022dccaeb7a4dd8c
GergoUjhelyi <ujhelyi.gergo@ericsson.com>
2017-08-16 14:47:04 +0200

    substr for bitstring element
    Signed-off-by: GergoUjhelyi <ujhelyi.gergo@ericsson.com>

--- org.eclipse.titan.runtime/src/org/eclipse/titan/runtime/core/AdditionalFunctions.java
+++ org.eclipse.titan.runtime/src/org/eclipse/titan/runtime/core/AdditionalFunctions.java
@@ -902,4 +902,34 @@ public class AdditionalFunctions {
 
 		return subString(value, idx.getInt(), returncount.getInt());
 	}
+	
+	public static TitanBitString subString(final TitanBitString_Element value, int idx, int returncount) {
+		value.mustBound("The first argument (value) of function substr() is an unbound bitstring element.");
+		
+		check_substr_arguments(idx, returncount, "bitstring", "bit");
+		if(returncount == 0) {
+			return new TitanBitString();
+		} else {
+			return new TitanBitString(value.get_bit() ? "1" : "0");
+		}
+	}
+	
+	public static TitanBitString subString(final TitanBitString_Element value, int idx, final TitanInteger returncount) {
+		returncount.mustBound("The third argument (returncount) of function substr() is an unbound integer value.");
+		
+		return subString(value, idx, returncount.getInt());
+	}
+	
+	public static TitanBitString subString(final TitanBitString_Element value, final TitanInteger idx, int returncount) {
+		idx.mustBound("The second argument (index) of function substr() is an unbound integer value.");
+		
+		return subString(value, idx.getInt(), returncount);
+	}
+	
+	public static TitanBitString subString(final TitanBitString_Element value, final TitanInteger idx, final TitanInteger returncount) {
+		idx.mustBound("The second argument (index) of function substr() is an unbound integer value.");
+		returncount.mustBound("The third argument (returncount) of function substr() is an unbound integer value.");
+		
+		return subString(value, idx.getInt(), returncount.getInt());
+	}
 }
