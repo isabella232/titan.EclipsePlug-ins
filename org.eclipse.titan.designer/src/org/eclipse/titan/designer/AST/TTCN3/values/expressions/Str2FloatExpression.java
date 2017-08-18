52621bbb058f83cb1d09c8744aab857eff315047
GergoUjhelyi <ujhelyi.gergo@ericsson.com>
2017-08-18 12:35:05 +0200

    AdditionalFunctions: add str2float and fixed warning messages
    Signed-off-by: GergoUjhelyi <ujhelyi.gergo@ericsson.com>

--- org.eclipse.titan.designer/src/org/eclipse/titan/designer/AST/TTCN3/values/expressions/Str2FloatExpression.java
+++ org.eclipse.titan.designer/src/org/eclipse/titan/designer/AST/TTCN3/values/expressions/Str2FloatExpression.java
@@ -23,6 +23,7 @@ import org.eclipse.titan.designer.AST.TTCN3.Expected_Value_type;
 import org.eclipse.titan.designer.AST.TTCN3.values.Charstring_Value;
 import org.eclipse.titan.designer.AST.TTCN3.values.Expression_Value;
 import org.eclipse.titan.designer.AST.TTCN3.values.Real_Value;
+import org.eclipse.titan.designer.compiler.JavaGenData;
 import org.eclipse.titan.designer.parsers.CompilationTimeStamp;
 import org.eclipse.titan.designer.parsers.ttcn3parser.ReParseException;
 import org.eclipse.titan.designer.parsers.ttcn3parser.TTCN3ReparseUpdater;
@@ -244,5 +245,15 @@ public final class Str2FloatExpression extends Expression_Value {
 			return false;
 		}
 		return true;
+	}
+	
+	@Override
+	/** {@inheritDoc} */
+	public void generateCodeExpressionExpression(final JavaGenData aData, final ExpressionStruct expression) {
+		aData.addCommonLibraryImport("AdditionalFunctions");
+
+		expression.expression.append("AdditionalFunctions.str2float( ");
+		value.generateCodeExpressionMandatory(aData, expression);
+		expression.expression.append(" )");
 	}
 }
