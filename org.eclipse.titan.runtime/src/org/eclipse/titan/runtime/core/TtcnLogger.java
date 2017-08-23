9b3a031e893f2a86651c1df6ddc6919f1a484be6
kristof <Kristof.Szabados@ericsson.com>
2017-08-23 17:41:56 +0200

    minor code style changes
    Signed-off-by: kristof <Kristof.Szabados@ericsson.com>

--- org.eclipse.titan.runtime/src/org/eclipse/titan/runtime/core/TtcnLogger.java
+++ org.eclipse.titan.runtime/src/org/eclipse/titan/runtime/core/TtcnLogger.java
@@ -16,83 +16,85 @@ import java.util.List;
  * @author Andrea Palfi
  */
 public class TtcnLogger {
-	
-	enum matching_verbosity_t { VERBOSITY_COMPACT, VERBOSITY_FULL };
 
-	public enum Severity{
-				NOTHING_TO_LOG, 
-				ACTION_UNQUALIFIED,
-				DEFAULTOP_ACTIVATE,
-				DEFAULTOP_DEACTIVATE,
-				DEFAULTOP_EXIT,
-				DEFAULTOP_UNQUALIFIED, 
-				ERROR_UNQUALIFIED,
-				EXECUTOR_RUNTIME,
-				EXECUTOR_CONFIGDATA,
-				EXECUTOR_EXTCOMMAND,
-				EXECUTOR_COMPONENT, 
-				EXECUTOR_LOGOPTIONS,
-				EXECUTOR_UNQUALIFIED,
-				FUNCTION_RND,
-				FUNCTION_UNQUALIFIED,
-				PARALLEL_PTC, 
-				PARALLEL_PORTCONN,
-				PARALLEL_PORTMAP,
-				PARALLEL_UNQUALIFIED,
-				TESTCASE_START,
-				TESTCASE_FINISH, 
-				TESTCASE_UNQUALIFIED,
-				PORTEVENT_PQUEUE,
-				PORTEVENT_MQUEUE,
-				PORTEVENT_STATE,
-				PORTEVENT_PMIN,
-				PORTEVENT_PMOUT,
-				PORTEVENT_PCIN,
-				PORTEVENT_PCOUT,
-				PORTEVENT_MMRECV,
-				PORTEVENT_MMSEND,
-				PORTEVENT_MCRECV,
-				PORTEVENT_MCSEND,
-				PORTEVENT_DUALRECV,
-				PORTEVENT_DUALSEND,
-				PORTEVENT_UNQUALIFIED,
-				PORTEVENT_SETSTATE,
-				STATISTICS_VERDICT,
-				STATISTICS_UNQUALIFIED,
-				TIMEROP_READ,
-				TIMEROP_START,
-				TIMEROP_GUARD,
-				TIMEROP_STOP,
-				TIMEROP_TIMEOUT,
-				TIMEROP_UNQUALIFIED,
-				USER_UNQUALIFIED,
-				VERDICTOP_GETVERDICT,
-				VERDICTOP_SETVERDICT,
-				VERDICTOP_FINAL,
-				VERDICTOP_UNQUALIFIED,
-				WARNING_UNQUALIFIED,
-				// MATCHING and DEBUG should be at the end (not included in LOG_ALL)
-				MATCHING_DONE,
-				MATCHING_TIMEOUT,
-				MATCHING_PCSUCCESS,
-				MATCHING_PCUNSUCC,
-				MATCHING_PMSUCCESS,
-				MATCHING_PMUNSUCC,
-				MATCHING_MCSUCCESS,
-				MATCHING_MCUNSUCC,
-				MATCHING_MMSUCCESS,
-				MATCHING_MMUNSUCC,
-				MATCHING_PROBLEM,
-				MATCHING_UNQUALIFIED,
-				DEBUG_ENCDEC,
-				DEBUG_TESTPORT,
-				DEBUG_USER,
-				DEBUG_FRAMEWORK,
-				DEBUG_UNQUALIFIED,
-				NUMBER_OF_LOGSEVERITIES, // must follow the last individual severity
-				LOG_ALL_IMPORTANT
+	enum matching_verbosity_t {
+		VERBOSITY_COMPACT, VERBOSITY_FULL
+	};
+
+	public enum Severity {
+		NOTHING_TO_LOG,
+		ACTION_UNQUALIFIED,
+		DEFAULTOP_ACTIVATE,
+		DEFAULTOP_DEACTIVATE,
+		DEFAULTOP_EXIT,
+		DEFAULTOP_UNQUALIFIED,
+		ERROR_UNQUALIFIED,
+		EXECUTOR_RUNTIME,
+		EXECUTOR_CONFIGDATA,
+		EXECUTOR_EXTCOMMAND,
+		EXECUTOR_COMPONENT,
+		EXECUTOR_LOGOPTIONS,
+		EXECUTOR_UNQUALIFIED,
+		FUNCTION_RND,
+		FUNCTION_UNQUALIFIED,
+		PARALLEL_PTC,
+		PARALLEL_PORTCONN,
+		PARALLEL_PORTMAP,
+		PARALLEL_UNQUALIFIED,
+		TESTCASE_START,
+		TESTCASE_FINISH,
+		TESTCASE_UNQUALIFIED,
+		PORTEVENT_PQUEUE,
+		PORTEVENT_MQUEUE,
+		PORTEVENT_STATE,
+		PORTEVENT_PMIN,
+		PORTEVENT_PMOUT,
+		PORTEVENT_PCIN,
+		PORTEVENT_PCOUT,
+		PORTEVENT_MMRECV,
+		PORTEVENT_MMSEND,
+		PORTEVENT_MCRECV,
+		PORTEVENT_MCSEND,
+		PORTEVENT_DUALRECV,
+		PORTEVENT_DUALSEND,
+		PORTEVENT_UNQUALIFIED,
+		PORTEVENT_SETSTATE,
+		STATISTICS_VERDICT,
+		STATISTICS_UNQUALIFIED,
+		TIMEROP_READ,
+		TIMEROP_START,
+		TIMEROP_GUARD,
+		TIMEROP_STOP,
+		TIMEROP_TIMEOUT,
+		TIMEROP_UNQUALIFIED,
+		USER_UNQUALIFIED,
+		VERDICTOP_GETVERDICT,
+		VERDICTOP_SETVERDICT,
+		VERDICTOP_FINAL,
+		VERDICTOP_UNQUALIFIED,
+		WARNING_UNQUALIFIED,
+		// MATCHING and DEBUG should be at the end (not included in LOG_ALL)
+		MATCHING_DONE,
+		MATCHING_TIMEOUT,
+		MATCHING_PCSUCCESS,
+		MATCHING_PCUNSUCC,
+		MATCHING_PMSUCCESS,
+		MATCHING_PMUNSUCC,
+		MATCHING_MCSUCCESS,
+		MATCHING_MCUNSUCC,
+		MATCHING_MMSUCCESS,
+		MATCHING_MMUNSUCC,
+		MATCHING_PROBLEM,
+		MATCHING_UNQUALIFIED,
+		DEBUG_ENCDEC,
+		DEBUG_TESTPORT,
+		DEBUG_USER,
+		DEBUG_FRAMEWORK,
+		DEBUG_UNQUALIFIED,
+		NUMBER_OF_LOGSEVERITIES, // must follow the last individual severity
+		LOG_ALL_IMPORTANT
 	}
-	
+
 	public static final Severity sev_categories[]=
 	{
 		Severity.NOTHING_TO_LOG,//=0
@@ -112,8 +114,7 @@ public class TtcnLogger {
 		Severity.MATCHING_UNQUALIFIED,
 		Severity.DEBUG_UNQUALIFIED,
 	};
-		
-	
+
 	public static String severity_category_names[]={
 		"NULL",
 		"ACTION",
@@ -132,19 +133,19 @@ public class TtcnLogger {
 		"MATCHING",
 		"DEBUG",
 	};
-	
+
 	private static class log_event_struct{
 		StringBuilder buffer = new StringBuilder();
 		Severity severity;
 	}
-	
+
 	String logMatchBuffer = null;
 	int logMatchBufferLen=0;
 	int logMachBufferSize=0;
 	boolean logMachPrinted = false;
-	
+
 	static log_event_struct current_event;
-	
+
 	public static void begin_event(Severity severity){
 		current_event=new log_event_struct();
 		current_event.buffer = new StringBuilder();
@@ -154,50 +155,41 @@ public class TtcnLogger {
 			list.add(current_event.severity);
 		}
 	}
-	
+
 	public static void end_event(){
 		System.out.println(current_event.buffer);
 	}
-	
-	
+
 	public static void log_event( final String msg ) {
 		// TODO Auto-generated method stub
-
 	}
 
 	public static void log_event_str( final String string ) {
 		current_event.buffer.append(string);
-
 	}
 
 	public static void log_char( final char c ) {
 		// TODO Auto-generated method stub
-
 	}
 
 	public static void log_event_uninitialized() {
 		// TODO Auto-generated method stub
-
 	}
 
 	public static void log_hex( final byte aHexDigit ) {
 		// TODO Auto-generated method stub
-
 	}
 
 	public static void log_event_unbound() {
 		// TODO Auto-generated method stub
-		
 	}
 
 	public static void log_octet( final char aOctet ) {
 		// TODO Auto-generated method stub
-
 	}
 
 	public static void log_bit( final byte aBit ) {
 		// TODO Auto-generated method stub
-
 	}
 
 	public static matching_verbosity_t get_matching_verbosity() {
@@ -207,23 +199,19 @@ public class TtcnLogger {
 
 	public static void print_logmatch_buffer() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	public static void log_logmatch_info(String string) {
 		// TODO Auto-generated method stub
-		
 	}
 
 	public static void set_logmatch_buffer_len(int previous_size) {
 		// TODO Auto-generated method stub
-		
 	}
 
 	public static int get_logmatch_buffer_len() {
 		// TODO Auto-generated ,method stub
 		return 0;
 	}
-	
 }
-
