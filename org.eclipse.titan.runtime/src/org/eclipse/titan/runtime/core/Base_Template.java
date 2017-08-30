7f421ddc803d16683d99f3cab725a0aca4239996
Fakas Izabella Ingrid <farkas94ingi@gmail.com>
2017-08-30 09:59:33 +0200

    fixed valueOf function for Base_Template, TitanDefault_template and TitanTemplateArray.
    Signed-off-by: Fakas Izabella Ingrid <farkas94ingi@gmail.com>

--- org.eclipse.titan.runtime/src/org/eclipse/titan/runtime/core/Base_Template.java
+++ org.eclipse.titan.runtime/src/org/eclipse/titan/runtime/core/Base_Template.java
@@ -173,4 +173,5 @@ public abstract class Base_Template {
 	public abstract Base_Template assign(final Base_Type otherValue);
 	public abstract Base_Template assign(final Base_Template otherValue);
 	public abstract TitanBoolean match(final Base_Type otherValue, final boolean legacy);
+	public abstract Base_Type valueOf();
 }
