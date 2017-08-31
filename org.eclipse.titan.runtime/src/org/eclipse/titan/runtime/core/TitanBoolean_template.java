37a8322416b8b13341efb326d68fc36caab164dc
andreapalfi <andrea.palfi@ericsson.com>
2017-08-31 16:13:24 +0200

    implemented log() in TitanBoolean_template
    Signed-off-by: andreapalfi <andrea.palfi@ericsson.com>

--- org.eclipse.titan.runtime/src/org/eclipse/titan/runtime/core/TitanBoolean_template.java
+++ org.eclipse.titan.runtime/src/org/eclipse/titan/runtime/core/TitanBoolean_template.java
@@ -256,7 +256,36 @@ public class TitanBoolean_template extends Base_Template {
 
 		return value_list.get(listIndex);
 	}
-
+	
+	public void log(){
+		switch(templateSelection){
+		case SPECIFIC_VALUE: 
+			if (single_value.equals(single_value))
+			{
+				TtcnLogger.log_event_str(single_value.toString());
+			} else {
+				single_value.operatorNotEquals(single_value);
+			}
+			break;
+		case COMPLEMENTED_LIST:
+			TtcnLogger.log_event_str("complement");
+		case VALUE_LIST: 
+			TtcnLogger.log_char('(');
+			for (int i = 0; i < value_list.size(); i++) {
+				if(i > 0) {
+					TtcnLogger.log_event_str(", ");
+				}
+				value_list.get(i).log();
+			}
+			TtcnLogger.log_char(')');
+			break;
+		default:
+			log_generic();
+			break;
+		}
+		log_ifpresent();
+	}
+	
 	// originally is_present (with default parameter)
 	public TitanBoolean isPresent() {
 		return isPresent(false);
