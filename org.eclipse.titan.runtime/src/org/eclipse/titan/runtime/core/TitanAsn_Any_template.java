bb0aea1f4c725a4a6e9f7c59f9c6efe840412d56
kristof <Kristof.Szabados@ericsson.com>
2017-09-28 10:10:37 +0200

    it is easyer to support asn.1 any, than not to.
    Signed-off-by: kristof <Kristof.Szabados@ericsson.com>

---
+++ org.eclipse.titan.runtime/src/org/eclipse/titan/runtime/core/TitanAsn_Any_template.java
@@ -0,0 +1,17 @@
+/******************************************************************************
+ * Copyright (c) 2000-2017 Ericsson Telecom AB
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ ******************************************************************************/
+package org.eclipse.titan.runtime.core;
+
+/**
+ * ASN.1 any type template
+ *
+ * @author Kristof Szabados
+ */
+public class TitanAsn_Any_template extends TitanOctetString_template {
+  // does not need extra functionality
+}
