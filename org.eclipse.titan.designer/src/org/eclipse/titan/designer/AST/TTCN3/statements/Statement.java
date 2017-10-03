5ab0dc07629c6a2ddf5c94704baeebcf470467fe
kristof <Kristof.Szabados@ericsson.com>
2017-10-03 09:58:30 +0200

    organize imports was executed
    Signed-off-by: kristof <Kristof.Szabados@ericsson.com>

--- org.eclipse.titan.designer/src/org/eclipse/titan/designer/AST/TTCN3/statements/Statement.java
+++ org.eclipse.titan.designer/src/org/eclipse/titan/designer/AST/TTCN3/statements/Statement.java
@@ -11,10 +11,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.eclipse.titan.designer.AST.ASTNode;
+import org.eclipse.titan.designer.AST.GovernedSimple.CodeSectionType;
 import org.eclipse.titan.designer.AST.ILocateableNode;
 import org.eclipse.titan.designer.AST.Location;
 import org.eclipse.titan.designer.AST.NULL_Location;
-import org.eclipse.titan.designer.AST.GovernedSimple.CodeSectionType;
 import org.eclipse.titan.designer.AST.TTCN3.IAppendableSyntax;
 import org.eclipse.titan.designer.AST.TTCN3.IIncrementallyUpdateable;
 import org.eclipse.titan.designer.AST.TTCN3.definitions.Definition;
