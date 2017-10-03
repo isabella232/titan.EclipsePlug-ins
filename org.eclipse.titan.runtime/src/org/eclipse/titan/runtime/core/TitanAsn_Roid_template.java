8441ef149a7db75f7eeff578e71905a60ca932a2
kristof <Kristof.Szabados@ericsson.com>
2017-10-03 16:48:48 +0200

    ASN.1 referenced object identifiers are synonyms of object identifiers
    Signed-off-by: kristof <Kristof.Szabados@ericsson.com>

---
+++ org.eclipse.titan.runtime/src/org/eclipse/titan/runtime/core/TitanAsn_Roid_template.java
@@ -0,0 +1,19 @@
+/******************************************************************************
+ * Copyright (c) 2000-2017 Ericsson Telecom AB
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ ******************************************************************************/
+package org.eclipse.titan.runtime.core;
+
+/**
+ * Runtime class for ASN.1 relative object identifiers (objid) template
+ *
+ * These are synonyms of object ids.
+ *
+ * @author Gergo Ujhelyi
+ * */
+public class TitanAsn_Roid_template extends TitanObjectid_template {
+
+}
