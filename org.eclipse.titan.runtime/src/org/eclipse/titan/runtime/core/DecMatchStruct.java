1b27e8dc34f93e26d19c660f60855510aa7f9aaa
zlovarp <arpad.lovassy@semcon.com>
2017-11-07 15:49:43 +0100

    missing files from previous commit
    Signed-off-by: zlovarp <arpad.lovassy@semcon.com>

---
+++ org.eclipse.titan.runtime/src/org/eclipse/titan/runtime/core/DecMatchStruct.java
@@ -0,0 +1,15 @@
+package org.eclipse.titan.runtime.core;
+
+/**
+ * Structure for storing a decoded content matching mechanism 
+ * (for bitstrings, hexstrings, octetstrings and charstrings)
+ * @author Arpad Lovassy
+ */
+class DecMatchStruct {
+
+	private IDecMatch instance;
+
+	public void log() {
+		// TODO Auto-generated method stub
+	}
+}
---
+++ org.eclipse.titan.runtime/src/org/eclipse/titan/runtime/core/IDecMatch.java
@@ -0,0 +1,31 @@
+package org.eclipse.titan.runtime.core;
+
+/**
+ * Interface/base class for decoded content matching
+ * <br>
+ * For every decmatch template the compiler generates a new class that inherits
+ * this one and implements its virtual functions. An instance of the new class
+ * is stored in the template object, which calls the appropriate virtual
+ * functions when the template object's match() or log() functions are called.
+ * 
+ * originally Dec_Match_Interface
+ * @author Arpad Lovassy
+ */
+interface IDecMatch {
+	//TODO
+	//public boolean match(TTCN_Buffer aBuffer);
+
+	public void log();
+	/** this returns the decoding result of the last successfully matched value,
+	 * which may be used by value and parameter redirect classes for optimization
+	 * (so they don't have to decode the same value again)
+	 * the function returns a void pointer (since the decoding could result in a
+	 * value of any type), which is converted to the required type when used */
+	public void get_dec_res();
+
+	/** this returns the decoded type's descriptor, which may be used by value and
+	 * parameter redirect classes to determine whether the redirected value would
+	 * be decoded into the same type as the type used in this decmatch template */
+	//TODO
+	//public final TTCN_Typedescriptor_t get_type_descr();
+};
