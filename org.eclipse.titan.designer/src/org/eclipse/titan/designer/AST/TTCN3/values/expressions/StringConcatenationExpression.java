6f06b0256ca0601584f38f6d861bc64a552880ec
balaskoa <jeno.balasko@ericsson.com>
2017-11-13 14:49:18 +0100

    Bugfix of Bug 527196 (Cannot determine the argument type of 'lengthof()' op)
    Signed-off-by: balaskoa <jeno.balasko@ericsson.com>

--- org.eclipse.titan.designer/src/org/eclipse/titan/designer/AST/TTCN3/values/expressions/StringConcatenationExpression.java
+++ org.eclipse.titan.designer/src/org/eclipse/titan/designer/AST/TTCN3/values/expressions/StringConcatenationExpression.java
@@ -180,9 +180,12 @@ public final class StringConcatenationExpression extends Expression_Value {
 				return myGovernor;
 			}
 		}
-
+		value1.setLoweridToReference(timestamp);
 		final IType v1_gov = value1.getExpressionGovernor(timestamp, expectedValue);
+
+		value2.setLoweridToReference(timestamp);
 		final IType v2_gov = value2.getExpressionGovernor(timestamp, expectedValue);
+
 		if (v1_gov != null) {
 			if (v2_gov != null) {
 				if (v1_gov.isCompatible(timestamp, v2_gov, null, null, null)) {
