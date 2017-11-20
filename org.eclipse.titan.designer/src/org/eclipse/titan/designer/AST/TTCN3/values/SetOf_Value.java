682ad4337d90a7186d890a0b8b4a494fb4cc128a
kristof <Kristof.Szabados@ericsson.com>
2017-11-20 15:08:45 +0100

    lets use objects for indexing
    Signed-off-by: kristof <Kristof.Szabados@ericsson.com>

--- org.eclipse.titan.designer/src/org/eclipse/titan/designer/AST/TTCN3/values/SetOf_Value.java
+++ org.eclipse.titan.designer/src/org/eclipse/titan/designer/AST/TTCN3/values/SetOf_Value.java
@@ -651,7 +651,7 @@ public final class SetOf_Value extends Value {
 						source.append(MessageFormat.format("{0} {1} = {2}.getAt({3});\n", ofTypeName, tempId1, name, ((Integer_Value) index).getValue()));
 					} else {
 						final String tempId2 = aData.getTemporaryVariableName();
-						source.append(MessageFormat.format("int {0};\n", tempId2));
+						source.append(MessageFormat.format("TitanInteger {0} = new TitanInteger();\n", tempId2));
 						index.generateCodeInit(aData, source, tempId2);
 						source.append(MessageFormat.format("{0} {1} = {2}.getAt({3});\n", ofTypeName, tempId1, name, tempId2));
 					}
