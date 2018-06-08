52e54c0ceaf256cc8a335dea345a27ed8bcc8dec
kristof <Kristof.Szabados@ericsson.com>
2018-06-08 12:59:38 +0200

    header fixes
    Signed-off-by: kristof <Kristof.Szabados@ericsson.com>

--- org.eclipse.titanium.refactoring/src/org/eclipse/titanium/refactoring/select_union/ChangeCreator.java
+++ org.eclipse.titanium.refactoring/src/org/eclipse/titanium/refactoring/select_union/ChangeCreator.java
@@ -1,3 +1,10 @@
+/******************************************************************************
+ * Copyright (c) 2000-2018 Ericsson Telecom AB
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ ******************************************************************************/
 package org.eclipse.titanium.refactoring.select_union;
 
 import java.util.ArrayList;
--- org.eclipse.titanium.refactoring/src/org/eclipse/titanium/refactoring/select_union/ChangeToSelectUnionRefactoring.java
+++ org.eclipse.titanium.refactoring/src/org/eclipse/titanium/refactoring/select_union/ChangeToSelectUnionRefactoring.java
@@ -1,4 +1,12 @@
+/******************************************************************************
+ * Copyright (c) 2000-2018 Ericsson Telecom AB
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ ******************************************************************************/
 package org.eclipse.titanium.refactoring.select_union;
+
 import java.text.MessageFormat;
 import java.util.HashSet;
 import java.util.Iterator;
