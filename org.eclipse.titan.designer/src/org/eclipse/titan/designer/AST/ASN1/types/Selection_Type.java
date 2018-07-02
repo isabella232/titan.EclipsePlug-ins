6c1a982a7f48e317a2f89f6f7b413bf47d135d96
ekrisza <ekrisza@HU00060815.ericsson.se>
2018-07-02 10:33:42 +0200

    now we can easily add support for redirections.
    Signed-off-by: ekrisza <ekrisza@HU00060815.ericsson.se>

--- org.eclipse.titan.designer/src/org/eclipse/titan/designer/AST/ASN1/types/Selection_Type.java
+++ org.eclipse.titan.designer/src/org/eclipse/titan/designer/AST/ASN1/types/Selection_Type.java
@@ -357,13 +357,14 @@ public final class Selection_Type extends ASN1Type implements IReferencingType {
 				break;
 			case TYPE_SIGNATURE:
 				source.append(MessageFormat.format("\tpublic static class {0}_call extends {1}_call '{' '}'\n", genName, referencedLast.getGenNameValue(aData, source, myScope)));
+				source.append(MessageFormat.format("\tpublic static class {0}_call_redirect extends {1}_call_redirect '{' '}'\n", genName, referencedLast.getGenNameValue(aData, source, myScope)));
 				if (!((Signature_Type) last).isNonblocking()) {
 					source.append(MessageFormat.format("\tpublic static class {0}_reply extends {1}_reply '{' '}'\n", genName, referencedLast.getGenNameValue(aData, source, myScope)));
+					source.append(MessageFormat.format("\tpublic static class {0}_reply_redirect extends {1}_reply_redirect '{' '}'\n", genName, referencedLast.getGenNameValue(aData, source, myScope)));
 				}
 				if (((Signature_Type) last).getSignatureExceptions() != null) {
 					source.append(MessageFormat.format("\tpublic static class {0}_template extends {1} '{' '}'\n", genName, referencedLast.getGenNameTemplate(aData, source, myScope)));
 				}
-				//FIXME *-redirect -s are missing
 				break;
 			default:
 				source.append(MessageFormat.format("\tpublic static class {0} extends {1} '{' '}'\n", ownName, referencedLast.getGenNameValue(aData, source, myScope)));
--- org.eclipse.titan.designer/src/org/eclipse/titan/designer/AST/TTCN3/types/Referenced_Type.java
+++ org.eclipse.titan.designer/src/org/eclipse/titan/designer/AST/TTCN3/types/Referenced_Type.java
@@ -772,13 +772,14 @@ public final class Referenced_Type extends ASN1Type implements IReferencingType 
 				break;
 			case TYPE_SIGNATURE:
 				source.append(MessageFormat.format("\tpublic static class {0}_call extends {1}_call '{' '}'\n", genName, getGenNameValue(aData, source, myScope)));
+				source.append(MessageFormat.format("\tpublic static class {0}_call_redirect extends {1}_call_redirect '{' '}'\n", genName, getGenNameValue(aData, source, myScope)));
 				if (!((Signature_Type) last).isNonblocking()) {
 					source.append(MessageFormat.format("\tpublic static class {0}_reply extends {1}_reply '{' '}'\n", genName, getGenNameValue(aData, source, myScope)));
+					source.append(MessageFormat.format("\tpublic static class {0}_reply_redirect extends {1}_reply_redirect '{' '}'\n", genName, getGenNameValue(aData, source, myScope)));
 				}
 				if (((Signature_Type) last).getSignatureExceptions() != null) {
 					source.append(MessageFormat.format("\tpublic static class {0}_template extends {1} '{' '}'\n", genName, getGenNameTemplate(aData, source, myScope)));
 				}
-				//FIXME *-redirect -s are missing
 				break;
 			default:
 				source.append(MessageFormat.format("\tpublic static class {0} extends {1} '{' '}'\n", ownName, getGenNameValue(aData, source, myScope)));
