30d0669da0158bd873e5458e323e629709fdb910
ekrisza <ekrisza@HU00060815.ericsson.se>
2018-07-13 10:54:33 +0200

    header comments were missing (and by now only some specifically marked features are missing)
    Signed-off-by: ekrisza <ekrisza@HU00060815.ericsson.se>

--- org.eclipse.titan.designer/src/org/eclipse/titan/designer/AST/TTCN3/types/PortGenerator.java
+++ org.eclipse.titan.designer/src/org/eclipse/titan/designer/AST/TTCN3/types/PortGenerator.java
@@ -1,3 +1,10 @@
+/******************************************************************************
+ * Copyright (c) 2000-2018 Ericsson Telecom AB
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v2.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/org/documents/epl-2.0/EPL-2.0.html
+ ******************************************************************************/
 package org.eclipse.titan.designer.AST.TTCN3.types;
 
 import java.text.MessageFormat;
@@ -7,7 +14,11 @@ import java.util.HashSet;
 import org.eclipse.titan.designer.AST.Location;
 import org.eclipse.titan.designer.compiler.JavaGenData;
 
-// FIXME translation ports are not yet supported
+/**
+ * The code generator part for port types.
+ * 
+ * @author Kristof Szabados
+ * */
 public class PortGenerator {
 
 	// The kind of the testport
