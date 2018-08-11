c3df6020c728b676de07eeb4533bc381d3582ac9
Kristof Szabados <Kristof.Szabados@ericsson.com>
2018-08-11 17:38:27 +0200

    comments.
    Signed-off-by: Kristof Szabados <Kristof.Szabados@ericsson.com>

--- org.eclipse.titan.designer/src/org/eclipse/titan/designer/actions/MakeCliArchive.java
+++ org.eclipse.titan.designer/src/org/eclipse/titan/designer/actions/MakeCliArchive.java
@@ -1,3 +1,10 @@
+/******************************************************************************
+ * Copyright (c) 2000-2018 Ericsson Telecom AB
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v2.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/org/documents/epl-2.0/EPL-2.0.html
+ ******************************************************************************/
 package org.eclipse.titan.designer.actions;
 
 import java.io.File;
@@ -29,6 +36,9 @@ import org.eclipse.ui.IObjectActionDelegate;
 import org.eclipse.ui.IWorkbenchPart;
 import org.eclipse.ui.handlers.HandlerUtil;
 
+/**
+ * Plug-in action to archive the contents of TITAN projects, without using the command line.
+ */
 public class MakeCliArchive extends AbstractHandler implements IObjectActionDelegate {
 
 	private static final String BIN_SUBPATH = File.separator + "bin";
