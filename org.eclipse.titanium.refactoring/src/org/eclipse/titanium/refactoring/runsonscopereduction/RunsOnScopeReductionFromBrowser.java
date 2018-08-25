0b07344d7c0caf524d036488df93e97e7c956bf6
Kristof Szabados <Kristof.Szabados@ericsson.com>
2018-08-25 18:07:31 +0200

    missing legal headers added.
    Signed-off-by: Kristof Szabados <Kristof.Szabados@ericsson.com>

--- org.eclipse.titanium.refactoring/src/org/eclipse/titanium/refactoring/runsonscopereduction/RunsOnScopeReductionFromBrowser.java
+++ org.eclipse.titanium.refactoring/src/org/eclipse/titanium/refactoring/runsonscopereduction/RunsOnScopeReductionFromBrowser.java
@@ -1,3 +1,10 @@
+/******************************************************************************
+ * Copyright (c) 2000-2018 Ericsson Telecom AB
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ ******************************************************************************/
 package org.eclipse.titanium.refactoring.runsonscopereduction;
 
 import java.util.Set;
@@ -18,6 +25,10 @@ import org.eclipse.ui.IObjectActionDelegate;
 import org.eclipse.ui.IWorkbenchPart;
 import org.eclipse.ui.handlers.HandlerUtil;
 
+/** 
+ * 
+ * @author Farkas Izabella Ingrid 
+ */
 public class RunsOnScopeReductionFromBrowser extends AbstractHandler implements IObjectActionDelegate{
 	private ISelection selection;
 
