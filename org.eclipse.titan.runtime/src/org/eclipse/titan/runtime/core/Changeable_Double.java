d38cc518e02458cbcfe40e0512fcfcbf9d71a538
Kristof Szabados <Kristof.Szabados@ericsson.com>
2018-09-02 09:03:47 +0200

    missed the legal header.
    Signed-off-by: Kristof Szabados <Kristof.Szabados@ericsson.com>

--- org.eclipse.titan.runtime/src/org/eclipse/titan/runtime/core/Changeable_Double.java
+++ org.eclipse.titan.runtime/src/org/eclipse/titan/runtime/core/Changeable_Double.java
@@ -1,7 +1,14 @@
+/******************************************************************************
+ * Copyright (c) 2000-2018 Ericsson Telecom AB
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v2.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/org/documents/epl-2.0/EPL-2.0.html
+ ******************************************************************************/
 package org.eclipse.titan.runtime.core;
 
 /**
- * Helper calss for doubles, when we they need to be updated inside functions.
+ * Helper class for doubles, when we they need to be updated inside functions.
  *
  * @author Kristof Szabados
  * */
