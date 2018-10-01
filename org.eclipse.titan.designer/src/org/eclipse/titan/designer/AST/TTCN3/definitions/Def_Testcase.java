995da8fd2023cdd6dd9e94299f04bc8ea1d3c6c8
Kristof Szabados <Kristof.Szabados@ericsson.com>
2018-10-01 07:15:44 +0200

    missing import.
    Signed-off-by: Kristof Szabados <Kristof.Szabados@ericsson.com>

--- org.eclipse.titan.designer/src/org/eclipse/titan/designer/AST/TTCN3/definitions/Def_Testcase.java
+++ org.eclipse.titan.designer/src/org/eclipse/titan/designer/AST/TTCN3/definitions/Def_Testcase.java
@@ -610,6 +610,8 @@ public final class Def_Testcase extends Definition implements IParameterisedAssi
 		sb.append(source);
 
 		if (formalParList == null || formalParList.getNofParameters() == 0) {
+			aData.addBuiltinTypeImport( "Ttcn3Float" );
+
 			final StringBuilder executeAllTestcases = aData.getExecuteAllTestcase();
 			executeAllTestcases.append(MessageFormat.format("{0}(false, new TitanFloat( new Ttcn3Float( 0.0 ) ));\n", getGenNameFromScope(aData, source, myScope, "testcase_")));
 		}
@@ -617,6 +619,8 @@ public final class Def_Testcase extends Definition implements IParameterisedAssi
 		final StringBuilder executeTestcase = aData.getExecuteTestcase();
 		executeTestcase.append("if(\"").append(identifier.getDisplayName()).append("\".equals(tescase_name)) {\n");
 		if (formalParList == null || formalParList.getNofParameters() == 0) {
+			aData.addBuiltinTypeImport( "Ttcn3Float" );
+
 			executeTestcase.append(MessageFormat.format("{0}(false, new TitanFloat( new Ttcn3Float( 0.0 ) ));\n", getGenNameFromScope(aData, source, myScope, "testcase_")));
 		} else {
 			executeTestcase.append("throw new TtcnError(MessageFormat.format(\"Test case {0} in module {1} cannot be executed individually (without control part) because it has parameters.\", tescase_name, name));\n");
