5408b17e1ffee53f544605788a660ebacec957c6
zlovarp <arpad.lovassy@semcon.com>
2018-10-10 14:13:00 +0200

    runtime module params bugfix 2
    Signed-off-by: zlovarp <arpad.lovassy@semcon.com>

--- org.eclipse.titan.runtime/src/org/eclipse/titan/runtime/core/Param_Types.java
+++ org.eclipse.titan.runtime/src/org/eclipse/titan/runtime/core/Param_Types.java
@@ -118,7 +118,7 @@ public final class Param_Types {
 		}
 
 		public void set_id(final Module_Param_Id p_id) {
-			if (p_id == null) {
+			if (id != null) {
 				throw new TtcnError("Internal error: Module_Param.set_id()");
 			}
 			id = p_id;
