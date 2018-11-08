792a1cdb7ced0a85d6b9442bfb722322ce48e6db
Kristof Szabados <Kristof.Szabados@ericsson.com>
2018-11-08 18:10:14 +0100

    that's already handled.
    Signed-off-by: Kristof Szabados <Kristof.Szabados@ericsson.com>

--- org.eclipse.titan.runtime/src/org/eclipse/titan/runtime/core/Runtime_Single_main.java
+++ org.eclipse.titan.runtime/src/org/eclipse/titan/runtime/core/Runtime_Single_main.java
@@ -45,7 +45,7 @@ public final class Runtime_Single_main {
 		try {
 			TTCN_Logger.log_executor_runtime(TitanLoggerApi.ExecutorRuntime_reason.enum_type.executor__start__single__mode);
 			Module_List.pre_init_modules();
-			//FIXME log_config_data
+
 			TTCN_Runtime.set_logger_parameters();
 			TTCN_Logger.open_file();
 			TTCN_Logger.write_logger_settings();
