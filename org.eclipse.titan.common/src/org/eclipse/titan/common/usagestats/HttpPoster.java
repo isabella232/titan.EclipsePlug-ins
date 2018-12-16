efbadcee45b20bb62eb469e432d9a262b11ff084
Kristof Szabados <Kristof.Szabados@ericsson.com>
2018-12-16 12:12:08 +0100

    add extra comment.
    Signed-off-by: Kristof Szabados <Kristof.Szabados@ericsson.com>

--- org.eclipse.titan.common/src/org/eclipse/titan/common/usagestats/HttpPoster.java
+++ org.eclipse.titan.common/src/org/eclipse/titan/common/usagestats/HttpPoster.java
@@ -32,7 +32,7 @@ public class HttpPoster {
 	}
 
 	public void post(final Map<String, String> data) {
-		//TODO evaluate why we need 3 tries with special numbers.
+		//the first three tries are "named" so that they can be opened on firewalls in labs.
 		final int[] ports = {49555, 59555, 61555, 0};
 		final Socket socket = new Socket();
 
