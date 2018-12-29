ba278a717657adbcd16578f3031fe66f94f902ba
Kristof Szabados <Kristof.Szabados@ericsson.com>
2018-12-29 13:03:47 +0100

    more readable check.
    Signed-off-by: Kristof Szabados <Kristof.Szabados@ericsson.com>

--- org.eclipse.titan.log.viewer/src/org/eclipse/titan/log/viewer/models/LogFileMetaData.java
+++ org.eclipse.titan.log.viewer/src/org/eclipse/titan/log/viewer/models/LogFileMetaData.java
@@ -236,7 +236,7 @@ public class LogFileMetaData implements Serializable {
 	 */
 	public void setOption(final String option) {
 		this.option = option;
-		if ((option != null) && (option.trim().length() > 0)) {
+		if ((option != null) && (!option.trim().isEmpty())) {
 			final String[] strings = option.split(";"); //$NON-NLS-1$
 			for (final String string : strings) {
 				final String[] values = string.split(":="); //$NON-NLS-1$
