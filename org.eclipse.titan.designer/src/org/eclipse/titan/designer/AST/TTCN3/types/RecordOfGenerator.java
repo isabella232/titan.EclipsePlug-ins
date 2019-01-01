914ca19544829b36f93230bd1e80304dab01a9f1
Kristof Szabados <Kristof.Szabados@ericsson.com>
2019-01-01 12:49:26 +0100

    fixed typo.
    Signed-off-by: Kristof Szabados <Kristof.Szabados@ericsson.com>

--- org.eclipse.titan.designer/src/org/eclipse/titan/designer/AST/TTCN3/types/RecordOfGenerator.java
+++ org.eclipse.titan.designer/src/org/eclipse/titan/designer/AST/TTCN3/types/RecordOfGenerator.java
@@ -773,7 +773,7 @@ public final class RecordOfGenerator {
 			source.append("\t\t * Returns the number of elements, that is, the largest used index plus\n");
 			source.append("\t\t * one and zero for the empty value.\n");
 			source.append("\t\t *\n");
-			source.append("\t\t * size_of in the core\n");
+			source.append("\t\t * lengthof in the core\n");
 			source.append("\t\t *\n");
 			source.append("\t\t * @return the number of elements.\n");
 			source.append("\t\t * */\n");
