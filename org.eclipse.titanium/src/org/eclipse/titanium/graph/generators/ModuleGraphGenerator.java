bb485c7493c284594a7a54c49b2a46b8983cec57
Kristof Szabados <Kristof.Szabados@ericsson.com>
2019-02-05 16:39:09 +0100

    lets extract dependency information via visitors, to reduce old interface size.
    Signed-off-by: Kristof Szabados <Kristof.Szabados@ericsson.com>

--- org.eclipse.titanium/src/org/eclipse/titanium/graph/generators/ModuleGraphGenerator.java
+++ org.eclipse.titanium/src/org/eclipse/titanium/graph/generators/ModuleGraphGenerator.java
@@ -8,19 +8,25 @@
 package org.eclipse.titanium.graph.generators;
 
 import java.awt.Color;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
+import java.util.TreeSet;
 
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.WorkspaceJob;
 import org.eclipse.titan.common.logging.ErrorReporter;
+import org.eclipse.titan.designer.AST.ASTVisitor;
+import org.eclipse.titan.designer.AST.IVisitableNode;
 import org.eclipse.titan.designer.AST.Identifier;
+import org.eclipse.titan.designer.AST.Module;
+import org.eclipse.titan.designer.AST.ModuleImportation;
 import org.eclipse.titan.designer.core.ProjectBasedBuilder;
 import org.eclipse.titan.designer.parsers.GlobalParser;
-import org.eclipse.titan.designer.parsers.GlobalProjectStructureTracker;
 import org.eclipse.titan.designer.parsers.ProjectSourceParser;
-import org.eclipse.titan.designer.parsers.ProjectStructureDataCollector;
 import org.eclipse.titanium.error.ErrorHandler;
 import org.eclipse.titanium.graph.components.EdgeDescriptor;
 import org.eclipse.titanium.graph.components.NodeDescriptor;
@@ -76,39 +82,56 @@ public class ModuleGraphGenerator extends GraphGenerator {
 
 		final List<IProject> visitedProjects = ProjectBasedBuilder.getProjectBasedBuilder(project).getAllReachableProjects();
 		final Map<String, Identifier> globalKnownModules = new HashMap<String, Identifier>();
+		final Map<String, Identifier> globalKnownModules2 = new HashMap<String, Identifier>();
 
+		// Collect the known module names
 		for (int i = 0; i < visitedProjects.size(); ++i) {
 			final IProject currentProject = visitedProjects.get(i);
-			//FIXME replace with visitor and remove (this is the last usage)
-			final ProjectStructureDataCollector collector = GlobalProjectStructureTracker.getDataCollector(currentProject);
-			collector.evaulateMissingModules();
+			final ProjectSourceParser projectSourceParser = GlobalParser.getProjectSourceParser(currentProject);
+			final Set<String> knownModuleNames = projectSourceParser.getKnownModuleNames();
+			final List<Module> modules = new ArrayList<Module>();
+			for (final String moduleName : new TreeSet<String>(knownModuleNames)) {
+				Module module = projectSourceParser.getModuleByName(moduleName);
+				modules.add(module);
+				final Identifier moduleID = module.getIdentifier();
+				globalKnownModules2.put(moduleName, moduleID);
 
-			// adding known modules
-			for (final Identifier moduleName : collector.knownModules.values()) {
-				final NodeDescriptor actNode = new NodeDescriptor(moduleName.getDisplayName(), moduleName.getName(), currentProject,
-						false, moduleName.getLocation());
-				globalKnownModules.put(moduleName.getName(), moduleName);
+				// add known modules
+				final NodeDescriptor actNode = new NodeDescriptor(moduleID.getDisplayName(), moduleID.getName(), currentProject,
+						false, moduleID.getLocation());
 				if (!graph.containsVertex(actNode)) {
 					graph.addVertex(actNode);
 					labels.put(actNode.getName(), actNode);
 				}
 			}
-
-			// adding missing modules
-			for (final Identifier moduleName : collector.missingModules.values()) {
-				if (!globalKnownModules.containsKey(moduleName.getName())) {
-					final NodeDescriptor actNode = new NodeDescriptor(moduleName.getDisplayName(), moduleName.getName(),
-							currentProject, true, moduleName.getLocation());
-					if (!graph.containsVertex(actNode)) {
-						graph.addVertex(actNode);
-						labels.put(actNode.getName(), actNode);
-					}
-				}
+		}
+		for (int i = 0; i < visitedProjects.size(); ++i) {
+			final IProject currentProject = visitedProjects.get(i);
+			final ProjectSourceParser projectSourceParser = GlobalParser.getProjectSourceParser(currentProject);
+			final Set<String> knownModuleNames = projectSourceParser.getKnownModuleNames();
+			final List<Module> modules = new ArrayList<Module>();
+			for (final String moduleName : new TreeSet<String>(knownModuleNames)) {
+				Module module = projectSourceParser.getModuleByName(moduleName);
+				modules.add(module);
 			}
+			for (Module module : modules) {
+				final ModuleImportsCheck importsCheck = new ModuleImportsCheck();
+				module.accept(importsCheck);
+				for (ModuleImportation im : importsCheck.getImports()) {
+					final Identifier importIdentifier = im.getIdentifier();
+					if (!globalKnownModules.containsKey(importIdentifier.getName())) {
+						//add missing modules
+						final NodeDescriptor actNode = new NodeDescriptor(importIdentifier.getDisplayName(), importIdentifier.getName(),
+								currentProject, true, importIdentifier.getLocation());
+						if (!graph.containsVertex(actNode)) {
+							graph.addVertex(actNode);
+							labels.put(actNode.getName(), actNode);
+						}
+					}
 
-			// building edges
-			for (final String from : collector.importations.keySet()) {
-				for (final String to : collector.importations.get(from)) {
+					//add edges
+					final String from = module.getIdentifier().getName();
+					final String to = importIdentifier.getName();
 					final EdgeDescriptor edge = new EdgeDescriptor(from + "->" + to, Color.black);
 					// if(!graph.containsEdge(edge))
 					graph.addEdge(edge, labels.get(from), labels.get(to), EdgeType.DIRECTED);
@@ -117,4 +140,24 @@ public class ModuleGraphGenerator extends GraphGenerator {
 		}
 	}
 
+	class ModuleImportsCheck extends ASTVisitor {
+		private final Set<ModuleImportation> setOfModules = new HashSet<ModuleImportation>();
+
+		public ModuleImportsCheck() {
+			setOfModules.clear();
+		}
+
+		public Set<ModuleImportation> getImports() {
+			return setOfModules;
+		}
+
+		@Override
+		public int visit(final IVisitableNode node) {
+			if(node instanceof ModuleImportation){
+				ModuleImportation mod = (ModuleImportation) node;
+				setOfModules.add(mod);
+			}
+			return V_CONTINUE;
+		}
+	}
 }
