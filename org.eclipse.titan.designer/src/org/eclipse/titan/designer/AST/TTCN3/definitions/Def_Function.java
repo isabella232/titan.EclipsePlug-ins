ec7b4fb62b37476c9dda8748868b2a8eaded416e
Kristof Szabados <Kristof.Szabados@ericsson.com>
2019-02-15 18:13:19 +0100

    class name needed.
    Signed-off-by: Kristof Szabados <Kristof.Szabados@ericsson.com>

--- org.eclipse.titan.designer/src/org/eclipse/titan/designer/AST/TTCN3/definitions/Def_Function.java
+++ org.eclipse.titan.designer/src/org/eclipse/titan/designer/AST/TTCN3/definitions/Def_Function.java
@@ -1261,7 +1261,7 @@ public final class Def_Function extends Definition implements IParameterisedAssi
 				final String returnTypeDisplayName = returnType.getTypename();
 
 				startFunction.append(MessageFormat.format("final {0} ret_val = new {0}({1}({2}));\n", returnTypeName, genName, actualParList));
-				startFunction.append("TTCN_Logger.begin_event(Severity.PARALLEL_UNQUALIFIED);\n");
+				startFunction.append("TTCN_Logger.begin_event(TTCN_Logger.Severity.PARALLEL_UNQUALIFIED);\n");
 				startFunction.append(MessageFormat.format("TTCN_Logger.log_event_str(\"Function {0} returned {1} : \");\n", identifier.getDisplayName(), returnTypeDisplayName));
 				startFunction.append("ret_val.log();\n");
 				startFunction.append("TTCN_Logger.end_event();;\n");
