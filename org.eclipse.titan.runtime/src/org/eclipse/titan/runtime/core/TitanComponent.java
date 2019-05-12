6f47854e7c238fdea0a7294d06b0e90bbae2fd67
Kristof Szabados <Kristof.Szabados@ericsson.com>
2019-05-12 15:04:52 +0200

    some missing functions added.
    Signed-off-by: Kristof Szabados <Kristof.Szabados@ericsson.com>

--- org.eclipse.titan.runtime/src/org/eclipse/titan/runtime/core/TitanComponent.java
+++ org.eclipse.titan.runtime/src/org/eclipse/titan/runtime/core/TitanComponent.java
@@ -172,6 +172,32 @@ public class TitanComponent extends Base_Type {
 		throw new TtcnError(MessageFormat.format("Internal Error: value `{0}'' can not be cast to component reference", otherValue));
 	}
 
+	/**
+	 * Checks if the current value is not equivalent to the provided one.
+	 *
+	 * operator!= in the core
+	 *
+	 * @param otherValue
+	 *                the other value to check against.
+	 * @return {@code true} if the values are not equivalent.
+	 */
+	public boolean operator_not_equals(final int otherValue) {
+		return !operator_equals(otherValue);
+	}
+
+	/**
+	 * Checks if the current value is not equivalent to the provided one.
+	 *
+	 * operator!= in the core
+	 *
+	 * @param otherValue
+	 *                the other value to check against.
+	 * @return {@code true} if the values are not equivalent.
+	 */
+	public boolean operator_not_equals(final TitanComponent otherValue) {
+		return !operator_equals(otherValue);
+	}
+
 	// originally component cast operator
 	public int get_component() {
 		must_bound("Using the value of an unbound component reference.");
@@ -454,4 +480,38 @@ public class TitanComponent extends Base_Type {
 	}
 
 	//FIXME implement set_param
+
+	/**
+	 * Checks if the first value is equivalent to the second one.
+	 *
+	 * static operator== in the core
+	 *
+	 * @param left_value
+	 *                the first value.
+	 * @param right_value
+	 *                the second value to check against.
+	 * @return {@code true} if the values are equivalent.
+	 */
+	public static boolean operator_equals(final int left_value, final TitanComponent right_value) {
+		right_value.must_bound("Unbound right operand of float comparison.");
+
+		return right_value.operator_equals(left_value);
+	}
+
+	/**
+	 * Checks if the first value is not equivalent to the second one.
+	 *
+	 * static operator!= in the core
+	 *
+	 * @param left_value
+	 *                the first value.
+	 * @param right_value
+	 *                the second value to check against.
+	 * @return {@code true} if the values are not equivalent.
+	 */
+	public static boolean operator_not_equals(final int left_value, final TitanComponent right_value) {
+		right_value.must_bound("Unbound right operand of float comparison.");
+
+		return right_value.operator_not_equals(left_value);
+	}
 }
