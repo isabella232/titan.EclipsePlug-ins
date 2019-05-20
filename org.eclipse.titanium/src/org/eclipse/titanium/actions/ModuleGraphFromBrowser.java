470d1fc57dcab11867478c3b1930bc577cc4e6f2
Kristof Szabados <Kristof.Szabados@ericsson.com>
2019-05-20 09:17:43 +0200

    add legal headers.
    Signed-off-by: Kristof Szabados <Kristof.Szabados@ericsson.com>

--- org.eclipse.titanium/src/org/eclipse/titanium/actions/ModuleGraphFromBrowser.java
+++ org.eclipse.titanium/src/org/eclipse/titanium/actions/ModuleGraphFromBrowser.java
@@ -1,4 +1,10 @@
-
+/******************************************************************************
+ * Copyright (c) 2000-2019 Ericsson Telecom AB
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v2.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/org/documents/epl-2.0/EPL-2.0.html
+ ******************************************************************************/
 package org.eclipse.titanium.actions;
 
 import org.eclipse.core.commands.AbstractHandler;
@@ -33,7 +39,11 @@ import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.handlers.HandlerUtil;
 import org.eclipse.ui.part.FileEditorInput;
 
-
+/**
+*
+* @author Hoang Le My Anh
+*
+*/
 public class ModuleGraphFromBrowser extends AbstractHandler implements IObjectActionDelegate {
 	private ISelection selection;
 
--- org.eclipse.titanium/src/org/eclipse/titanium/actions/ModuleGraphFromEditor.java
+++ org.eclipse.titanium/src/org/eclipse/titanium/actions/ModuleGraphFromEditor.java
@@ -1,4 +1,10 @@
-
+/******************************************************************************
+ * Copyright (c) 2000-2019 Ericsson Telecom AB
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v2.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/org/documents/epl-2.0/EPL-2.0.html
+ ******************************************************************************/
 package org.eclipse.titanium.actions;
 
 import org.eclipse.core.commands.AbstractHandler;
@@ -29,6 +35,11 @@ import org.eclipse.ui.IWorkbenchPage;
 import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.part.FileEditorInput;
 
+/**
+ *
+ * @author Hoang Le My Anh
+ *
+ */
 public final class ModuleGraphFromEditor extends AbstractHandler{
 
 	@Override
