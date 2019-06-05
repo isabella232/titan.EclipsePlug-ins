bf3510164bddf0a259b3b6f5611a9898618d5c1b
Kristof Szabados <Kristof.Szabados@ericsson.com>
2019-06-05 17:42:18 +0200

    can be safely executed without checking referencing modules.
    Signed-off-by: Kristof Szabados <Kristof.Szabados@ericsson.com>

--- org.eclipse.titanium.refactoring/src/org/eclipse/titanium/refactoring/expandvaluelistnotation/ExpandFieldNamesRefactoring.java
+++ org.eclipse.titanium.refactoring/src/org/eclipse/titanium/refactoring/expandvaluelistnotation/ExpandFieldNamesRefactoring.java
@@ -90,7 +90,7 @@ public class ExpandFieldNamesRefactoring extends Refactoring {
 			// check that there are no ttcnpp files in the
 			// project
 			for (final IProject project : projects) {
-				if (GlobalParser.hasTtcnppFiles(project)) {//FIXME actually all referencing and referenced projects need to be checked too !
+				if (GlobalParser.hasTtcnppFiles(project)) {
 					result.addError(MessageFormat.format(PROJECTCONTAINSTTCNPPFILES, project));
 				}
 			}
