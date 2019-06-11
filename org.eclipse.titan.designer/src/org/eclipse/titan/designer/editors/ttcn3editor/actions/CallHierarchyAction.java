76ca64ea3d19abf0fc66c1f62e774dcfc1665a11
Sándor Balázs <it.sandor.balazs@gmail.com>
2019-06-11 22:53:19 +0200

    Implement CallHierarchyAction methods.
    Signed-off-by: Sándor Balázs <it.sandor.balazs@gmail.com>

--- org.eclipse.titan.designer/src/org/eclipse/titan/designer/editors/ttcn3editor/actions/CallHierarchyAction.java
+++ org.eclipse.titan.designer/src/org/eclipse/titan/designer/editors/ttcn3editor/actions/CallHierarchyAction.java
@@ -8,12 +8,16 @@
 package org.eclipse.titan.designer.editors.ttcn3editor.actions;
 
 import org.eclipse.ui.IEditorPart;
+import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.IEditorActionDelegate;
 import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.action.IStatusLineManager;
+import org.eclipse.jface.text.TextSelection;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.titan.designer.graphics.ImageCache;
 
 /**
  * <p>
@@ -28,29 +32,143 @@ import org.eclipse.core.commands.ExecutionException;
  * @author Sándor Bálazs
  * */
 public class CallHierarchyAction extends AbstractHandler implements IEditorActionDelegate {
-
+	/**
+	 * Store the current selection from the TTCN3 Editor at the time of the right click action.<br>
+	 * Updated in the {@link #selectionChanged(IAction, ISelection)}.
+	 */
+	private ISelection selection;
+	
+	/**
+	 * The current editor.<br>
+	 * Updated in the {@link #setActiveEditor(IAction, IEditorPart)}<br>
+	 * and in the {@link #CallHierarchyAction()}.
+	 */
+	private IEditorPart targetEditor;
+	
+	private static final String SHOW_VIEW_ERROR 		= "The \"Call Hierarchy\" view cannot be displayed.";
+	private static final String REFERENCE_SEARCH_FAILED = "The Call Hierarchy search failed.";
+	private static final int STATUS_LINE_LEVEL_MESSAGE	= 0;
+	private static final int STATUS_LINE_LEVEL_ERROR 	= 1;
+	private static final int STATUS_LINE_CLEAR 			= -1;
+	
+	/**
+	 * The <code>CallHierarchyAction</code> class constructor.<br>
+	 * Set the current selection empty.
+	 */
+	public CallHierarchyAction() {
+		selection 		= TextSelection.emptySelection();
+		targetEditor 	= PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage().getActiveEditor();
+	}
+	
+	/**
+	 * The run method react for the triggered action.
+	 * @see #processing()
+	 * @param action
+	 * 			The current triggered action.
+	 */
 	@Override
-	public void run(IAction action) {
-		// TODO Auto-generated method stub
-		
+	public void run(final IAction action) {
+		processing();
+	}
+	
+	/**
+	 * Always record the current selection.
+	 * @param action
+	 * 			The current triggered action.
+	 * @param selection
+	 * 			The new selection from the TTCN3 Editor.
+	 */
+	@Override
+	public void selectionChanged(final IAction action, final ISelection selection) {
+		this.selection = selection;
 	}
 
+	/**
+	 * Set the active targetEditor in this action ant the {@link CallHierarchyView} too.
+	 * @param action
+	 * 			The current triggered action.
+	 * @param selection
+	 * 			The new selection from the TTCN3 Editor.
+	 */
 	@Override
-	public void selectionChanged(IAction action, ISelection selection) {
-		// TODO Auto-generated method stub
-		
+	public void setActiveEditor(final IAction action, final IEditorPart activeEditor) {
+		targetEditor = activeEditor;
 	}
 
+	/**
+	 * The execute method react for the triggered event.
+	 * @see #processing()
+	 * @param event
+	 * 			The execute triggering event.
+	 */
 	@Override
-	public Object execute(ExecutionEvent event) throws ExecutionException {
-		// TODO Auto-generated method stub
+	public Object execute(final ExecutionEvent event) throws ExecutionException {
+		processing();
 		return null;
 	}
+	
+	/**
+	 * <p>
+	 * The real processing method.<br>
+	 * Show the {@link CallHierarchyView} and run the {@link CallHierarchy#functionCallFinder(ISelection)} reference search method.<br>
+	 * Then create the call hierarchy graph root and add the search results to the root and then update the {@link CallHierarchyView}.
+	 * </p>
+	 */
+	private void processing() {
 
-	@Override
-	public void setActiveEditor(IAction action, IEditorPart targetEditor) {
-		// TODO Auto-generated method stub
-		
 	}
-
+	
+	/**
+	 * Show message on the target editors status bar.<br>
+	 * The message level is automatically STATUS_LINE_LEVEL_MESSAGE.
+	 * 
+	 * @see #showStatusLineMessage(String, int)
+	 * @param message
+	 * 			The string of the message.
+	 */
+	public void showStatusLineMessage(final String message) {
+		showStatusLineMessage(message, STATUS_LINE_LEVEL_MESSAGE);
+	}
+	
+	/**
+	 * Show message on the target editors status bar.<br>
+	 * The message level possible ERROR OR MESSAGE. The level define by the level parameter.<br>
+	 * With the STATUS_LINE_CLEAR parameter, the method clear the status line.
+	 * 
+	 * @param message
+	 * 			The string of the message.
+	 * @param level
+	 * 			The level of message.<br>
+	 * 			Possible: STATUS_LINE_LEVEL_MESSAGE or STATUS_LINE_LEVEL_ERROR or STATUS_LINE_CLEAR
+	 */
+	public void showStatusLineMessage(final String message, final int level) {
+		if(targetEditor == null) {
+			return;
+		}
+		
+		IStatusLineManager statusLineManager = targetEditor.getEditorSite().getActionBars().getStatusLineManager();
+		if(statusLineManager == null) {
+			return;
+		}
+		
+		statusLineManager.setMessage(null);
+		statusLineManager.setErrorMessage(null);
+		
+		if(level == STATUS_LINE_LEVEL_MESSAGE) {
+			statusLineManager.setMessage(ImageCache.getImage("titan.gif"), message);
+		}
+		
+		if(level == STATUS_LINE_LEVEL_ERROR) {
+			statusLineManager.setErrorMessage(ImageCache.getImage("compiler_error_fresh.gif"), message);
+		}
+	}
+	
+	/**
+	 * Clear the target editors status bar.<br>
+	 * Use: {@link #showStatusLineMessage(String, int)}
+	 * @see {@link #showStatusLineMessage(String, int)}
+	 */
+	public void clearStatusLineMessage() {
+		showStatusLineMessage("", STATUS_LINE_CLEAR);
+	}
 }
