ef7003e4a8887fdaff4861e69b08b1536067fff2
Sándor Balázs <it.sandor.balazs@gmail.com>
2019-06-11 22:59:30 +0200

    Adding CallHierarchyContentProvider class.
    Signed-off-by: Sándor Balázs <it.sandor.balazs@gmail.com>

---
+++ org.eclipse.titan.designer/src/org/eclipse/titan/designer/editors/ttcn3editor/CallHierarchyContentProvider.java
@@ -0,0 +1,25 @@
+/******************************************************************************
+ * Copyright (c) 2000-2019 Ericsson Telecom AB
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v2.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/org/documents/epl-2.0/EPL-2.0.html
+ ******************************************************************************/
+package org.eclipse.titan.designer.editors.ttcn3editor;
+
+import org.eclipse.jface.viewers.Viewer;
+import org.eclipse.jface.viewers.ITreeContentProvider;
+import org.eclipse.titan.designer.editors.ttcn3editor.CallHierarchyNode;
+
+/**
+ * Content provider for the {@link CallHierarchyView}'s TreeViewer.<br>
+ * The content provider work on {@link CallHierarchyNode}s.
+
+ * @see CallHierarchyView
+ * @see CallHierarchyNode
+ * 
+ * @author Sándor Bálazs
+ */
+public class CallHierarchyContentProvider{
+	
+}
