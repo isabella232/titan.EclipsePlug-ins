f1b6e8bf58c811f96454adc9ec3f430543804985
Sándor Balázs <it.sandor.balazs@gmail.com>
2019-06-11 23:01:57 +0200

    Adding CallHierarchyLabelProvider class.
    Signed-off-by: Sándor Balázs <it.sandor.balazs@gmail.com>

---
+++ org.eclipse.titan.designer/src/org/eclipse/titan/designer/editors/ttcn3editor/CallHierarchyLabelProvider.java
@@ -0,0 +1,32 @@
+/******************************************************************************
+ * Copyright (c) 2000-2019 Ericsson Telecom AB
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v2.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/org/documents/epl-2.0/EPL-2.0.html
+ ******************************************************************************/
+package org.eclipse.titan.designer.editors.ttcn3editor;
+
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.jface.viewers.LabelProvider;
+import java.text.MessageFormat;
+import org.eclipse.jface.viewers.ITableLabelProvider;
+import org.eclipse.titan.designer.AST.Assignment;
+import org.eclipse.titan.designer.AST.Reference;
+import org.eclipse.titan.designer.graphics.ImageCache;
+import org.eclipse.titan.designer.parsers.CompilationTimeStamp;
+import org.eclipse.titan.designer.editors.ttcn3editor.CallHierarchyNode;
+
+/**
+ * <p>
+ * Label provider for the {@link CallHierarchyView}'s TreeViewer and TableViewer.<br>
+ * The label provider work on {@link CallHierarchyNode}s and references.
+ * </p>
+ * @see CallHierarchyView
+ * @see CallHierarchyNode
+ * 
+ * @author Sándor Bálazs
+ */
+public class CallHierarchyLabelProvider {
+	
+}
