a8aa5c4bb97a59921067dc48bf05893ceb30045d
Sándor Balázs <it.sandor.balazs@gmail.com>
2019-06-11 23:05:12 +0200

    CallHierarchyView extends ViewPart.
    Signed-off-by: Sándor Balázs <it.sandor.balazs@gmail.com>

--- org.eclipse.titan.designer/src/org/eclipse/titan/designer/editors/ttcn3editor/CallHierarchyView.java
+++ org.eclipse.titan.designer/src/org/eclipse/titan/designer/editors/ttcn3editor/CallHierarchyView.java
@@ -7,6 +7,47 @@
  ******************************************************************************/
 package org.eclipse.titan.designer.editors.ttcn3editor;
 
+import java.text.MessageFormat;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import org.eclipse.core.resources.IFile;
+import org.eclipse.jface.action.IStatusLineManager;
+import org.eclipse.jface.viewers.ArrayContentProvider;
+import org.eclipse.jface.viewers.ISelectionChangedListener;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.viewers.SelectionChangedEvent;
+import org.eclipse.jface.viewers.TreeViewer;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.custom.SashForm;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.ui.IEditorInput;
+import org.eclipse.ui.IEditorPart;
+import org.eclipse.ui.IEditorReference;
+import org.eclipse.ui.IViewPart;
+import org.eclipse.ui.PartInitException;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.part.FileEditorInput;
+import org.eclipse.ui.part.ViewPart;
+import org.eclipse.ui.texteditor.ITextEditor;
+import org.eclipse.swt.widgets.Table;
+import org.eclipse.swt.widgets.TableColumn;
+import org.eclipse.titan.designer.AST.Location;
+import org.eclipse.titan.designer.AST.Reference;
+import org.eclipse.titan.designer.graphics.ImageCache;
+import org.eclipse.titan.designer.productUtilities.ProductConstants;
+import org.eclipse.jface.viewers.TableLayout;
+import org.eclipse.jface.viewers.TableViewer;
+import org.eclipse.jface.viewers.ColumnLayoutData;
+import org.eclipse.jface.viewers.ColumnPixelData;
+import org.eclipse.jface.viewers.ColumnWeightData;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.swt.widgets.Listener;
+import org.eclipse.swt.widgets.Event;
+
 /**
  * <p>
  * The <code>CallHierarchyView</code> class represent the view for the call hierarchy data visualization.
@@ -17,6 +58,24 @@ package org.eclipse.titan.designer.editors.ttcn3editor;
  * @see ViewPart
  * @see ISelectionChangedListener
  */
-public class CallHierarchyView {
+public class CallHierarchyView extends ViewPart implements ISelectionChangedListener {
+
+	@Override
+	public void selectionChanged(SelectionChangedEvent event) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void createPartControl(Composite parent) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setFocus() {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
