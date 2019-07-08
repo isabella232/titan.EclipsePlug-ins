3857fb2e9574b7e1d47549ba2baead80e0224720
Kristof Szabados <Kristof.Szabados@ericsson.com>
2019-07-08 17:35:12 +0200

    removed extra line ending whitespace.
    Signed-off-by: Kristof Szabados <Kristof.Szabados@ericsson.com>

--- org.eclipse.titan.runtime/src/org/eclipse/titan/runtime/core/AdditionalFunctions.java
+++ org.eclipse.titan.runtime/src/org/eclipse/titan/runtime/core/AdditionalFunctions.java
@@ -5315,10 +5315,10 @@ public final class AdditionalFunctions {
 
 		return regexp(instr, expression, groupno.get_int(), nocase);
 	}
-	
+
 	public static TitanCharString regexp(final TitanUniversalCharString instr, final TitanCharString expression, final TitanInteger groupno, final boolean nocase) {
 		instr.must_bound("The first argument (instr) of function regexp() is an unbound universal charstring value.");
-		
+
 		if (instr.charstring) {
 			return regexp(new TitanCharString(instr.cstr), expression, groupno, nocase);
 		} else {
