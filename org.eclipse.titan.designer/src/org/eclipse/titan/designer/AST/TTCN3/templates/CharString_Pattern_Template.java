126cc3ba036d7251f00e8663873f186cdc9951ba
GergoUjhelyi <ujhelyi.gergo@ericsson.com>
2019-07-12 16:16:12 +0200

    fixed numbers
    Signed-off-by: GergoUjhelyi <ujhelyi.gergo@ericsson.com>

--- org.eclipse.titan.designer/src/org/eclipse/titan/designer/AST/TTCN3/templates/CharString_Pattern_Template.java
+++ org.eclipse.titan.designer/src/org/eclipse/titan/designer/AST/TTCN3/templates/CharString_Pattern_Template.java
@@ -270,7 +270,7 @@ public final class CharString_Pattern_Template extends TTCN3Template {
 
 		aData.addBuiltinTypeImport( "TitanCharString" );
 		aData.addBuiltinTypeImport( "Base_Template.template_sel" );
-		result.append( MessageFormat.format( "new {1}", myGovernor.getGenNameTemplate(aData, result),patternstring.create_charstring_literals(aData,myScope.getModuleScopeGen(),null)));	
+		result.append( MessageFormat.format( "new {0}", patternstring.create_charstring_literals(aData,myScope.getModuleScopeGen(),null)));	
 
 		//TODO handle cast needed
 
