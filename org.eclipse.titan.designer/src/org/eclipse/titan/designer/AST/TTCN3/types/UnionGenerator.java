30a307d50748c7d1118bd5fbf2614156ac23a2b8
Kristof Szabados <Kristof.Szabados@ericsson.com>
2019-08-08 18:20:45 +0200

    minor indentation improvement.
    Signed-off-by: Kristof Szabados <Kristof.Szabados@ericsson.com>

--- org.eclipse.titan.designer/src/org/eclipse/titan/designer/AST/TTCN3/types/UnionGenerator.java
+++ org.eclipse.titan.designer/src/org/eclipse/titan/designer/AST/TTCN3/types/UnionGenerator.java
@@ -1889,7 +1889,7 @@ public final class UnionGenerator {
 		source.append("\t\t@Override\n");
 		source.append("\t\tpublic int n_list_elem() {\n");
 		source.append("\t\t\tif (template_selection != template_sel.VALUE_LIST && template_selection != template_sel.COMPLEMENTED_LIST) {\n");
-		source.append(MessageFormat.format( "\t\t\tthrow new TtcnError(\"Internal error: Accessing a list element of a non-list template of union type {0}.\");\n", displayName ) );
+		source.append(MessageFormat.format( "\t\t\t\tthrow new TtcnError(\"Internal error: Accessing a list element of a non-list template of union type {0}.\");\n", displayName ) );
 		source.append("\t\t\t}\n");
 		source.append("\t\t\treturn value_list.size();\n");
 		source.append("\t\t}\n\n");
