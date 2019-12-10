e995adcc4b7fda8905c7d65024f629988829e363
zlovarp <arpad.lovassy@semcon.com>
2019-12-10 11:56:59 +0100

    missing imports
    Signed-off-by: zlovarp <arpad.lovassy@semcon.com>

--- org.eclipse.titan.designer/src/org/eclipse/titan/designer/AST/TTCN3/types/EnumeratedGenerator.java
+++ org.eclipse.titan.designer/src/org/eclipse/titan/designer/AST/TTCN3/types/EnumeratedGenerator.java
@@ -99,7 +99,9 @@ public final class EnumeratedGenerator {
 		aData.addBuiltinTypeImport("RAW.RAW_enc_tr_pos");
 		aData.addBuiltinTypeImport("RAW.RAW_enc_tree");
 		aData.addBuiltinTypeImport("RAW.top_bit_order_t");
+		aData.addBuiltinTypeImport("JSON_Tokenizer");
 		aData.addBuiltinTypeImport("TTCN_Buffer");
+		aData.addBuiltinTypeImport("TTCN_EncDec");
 		aData.addBuiltinTypeImport("TTCN_EncDec.coding_type");
 		aData.addBuiltinTypeImport("TTCN_EncDec.error_type");
 		aData.addBuiltinTypeImport("TTCN_EncDec.raw_order_t");
@@ -586,7 +588,6 @@ public final class EnumeratedGenerator {
 		if (jsonNeeded) {
 			aData.addImport("java.util.concurrent.atomic.AtomicInteger");
 			aData.addImport("java.util.concurrent.atomic.AtomicReference");
-			aData.addBuiltinTypeImport("JSON_Tokenizer");
 			aData.addBuiltinTypeImport("JSON_Tokenizer.json_token_t");
 
 			// JSON encode
