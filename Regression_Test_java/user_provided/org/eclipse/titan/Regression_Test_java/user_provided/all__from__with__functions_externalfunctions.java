209b97a44ec014f3aa0248722e159289e15b124f
balaskoa <Jeno.Balasko@ericsson.com>
2019-12-10 09:35:01 +0100

    json and all_from tests have been added to Regression_Test_java
    Signed-off-by: balaskoa <Jeno.Balasko@ericsson.com>

---
+++ Regression_Test_java/user_provided/org/eclipse/titan/Regression_Test_java/user_provided/all__from__with__functions_externalfunctions.java
@@ -0,0 +1,11 @@
+package org.eclipse.titan.Regression_Test_java.user_provided;
+
+import org.eclipse.titan.runtime.core.PreGenRecordOf.PREGEN__RECORD__OF__INTEGER_template;
+
+public class all__from__with__functions_externalfunctions {
+
+	public static PREGEN__RECORD__OF__INTEGER_template f__ext(PREGEN__RECORD__OF__INTEGER_template t_roi) {
+		return t_roi;
+	}
+
+}
