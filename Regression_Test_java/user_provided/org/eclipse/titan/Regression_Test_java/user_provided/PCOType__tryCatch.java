1a567b81f7ddae71d5657e81733af477c2023c04
balaskoa <Jeno.Balasko@ericsson.com>
2019-12-19 12:10:56 +0100

    tryCatch tests have been mapped into Regression_Test_java
    Signed-off-by: balaskoa <Jeno.Balasko@ericsson.com>

---
+++ Regression_Test_java/user_provided/org/eclipse/titan/Regression_Test_java/user_provided/PCOType__tryCatch.java
@@ -0,0 +1,19 @@
+package org.eclipse.titan.Regression_Test_java.user_provided;
+
+import org.eclipse.titan.Regression_Test_java.generated.tryCatch__Testcases.PCOType__tryCatch_BASE;
+import org.eclipse.titan.runtime.core.TitanCharString;
+
+public class PCOType__tryCatch extends PCOType__tryCatch_BASE {
+
+
+	public PCOType__tryCatch( final String port_name) {
+		super(port_name);
+	}
+	
+	@Override
+	protected void outgoing_send(TitanCharString send_par) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Regression_Test_java/user_provided/org/eclipse/titan/Regression_Test_java/user_provided/tryCatch__Testcases_externalfunctions.java
@@ -0,0 +1,12 @@
+package org.eclipse.titan.Regression_Test_java.user_provided;
+
+import org.eclipse.titan.runtime.core.TitanCharString;
+
+public class tryCatch__Testcases_externalfunctions {
+
+	public static void throw_(TitanCharString e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
