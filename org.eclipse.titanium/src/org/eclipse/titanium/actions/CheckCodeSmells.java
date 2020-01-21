70afbe3fe7faa5329d7bb3d65db741ab20d1e066
Kristof Szabados <Kristof.Szabados@ericsson.com>
2020-01-21 07:01:48 +0100

    removed unnecessary code.
    Signed-off-by: Kristof Szabados <Kristof.Szabados@ericsson.com>

--- org.eclipse.titanium/src/org/eclipse/titanium/actions/CheckCodeSmells.java
+++ org.eclipse.titanium/src/org/eclipse/titanium/actions/CheckCodeSmells.java
@@ -9,29 +9,20 @@ package org.eclipse.titanium.actions;
 
 import java.util.ArrayList;
 import java.util.Deque;
-import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Map;
 
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
-import org.eclipse.core.resources.IFile;
-import org.eclipse.core.resources.IFolder;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IResource;
-import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Platform;
 import org.eclipse.core.runtime.Status;
 import org.eclipse.core.runtime.SubMonitor;
 import org.eclipse.jface.viewers.ISelection;
-import org.eclipse.titan.common.logging.ErrorReporter;
-import org.eclipse.titan.designer.AST.Module;
-import org.eclipse.titan.designer.parsers.GlobalParser;
-import org.eclipse.titan.designer.parsers.ProjectSourceParser;
 import org.eclipse.titanium.Activator;
 import org.eclipse.titanium.markers.handler.MarkerHandler;
 import org.eclipse.titanium.markers.utils.Analyzer;
@@ -51,6 +42,7 @@ import org.eclipse.ui.PlatformUI;
  * whole, while for single files, only those files are analyzed.
  *
  * @author poroszd
+ * @author Kristof Szabados
  *
  */
 public class CheckCodeSmells extends AbstractHandler {
@@ -61,19 +53,13 @@ public class CheckCodeSmells extends AbstractHandler {
 
 		final List<IResource> res = org.eclipse.titan.common.utils.SelectionUtils.getResourcesFromSelection(selection);
 
-		final Map<IProject, List<IFile>> files = new HashMap<IProject, List<IFile>>();
 		final List<IProject> projects = new ArrayList<IProject>();
-		collectResourcesToBeAnalyzed(new LinkedList<IResource>(res), files, projects);
+		collectResourcesToBeAnalyzed(new LinkedList<IResource>(res), projects);
 
 		final String titaniumId = Activator.PLUGIN_ID;
 		final String onTheFlyPref = PreferenceConstants.ON_THE_FLY_SMELLS;
 		final boolean onTheFlyEnabled = Platform.getPreferencesService().getBoolean(titaniumId, onTheFlyPref, false, null);
 		final Analyzer analyzer = AnalyzerCache.withPreference();
-
-		//Clear the files, that will be analyzed inside projects.
-		for (final IProject project : projects) {
-			files.remove(project);
-		}
 		// check projects
 		checkProjects(projects, onTheFlyEnabled, analyzer);
 
@@ -82,35 +68,14 @@ public class CheckCodeSmells extends AbstractHandler {
 
 	/**
 	 * @param res the resources that have to be checked for potential files and projects
-	 * @param files the files found for analyzes
 	 * @param projects the projects found for analyzes
 	 */
-	private void collectResourcesToBeAnalyzed(final Deque<IResource> res, final Map<IProject, List<IFile>> files, final List<IProject> projects) {
+	private void collectResourcesToBeAnalyzed(final Deque<IResource> res, final List<IProject> projects) {
 		while (!res.isEmpty()) {
 			final IResource resource = res.pollFirst();
 			if (resource instanceof IProject) {
 				final IProject project = (IProject) resource;
 				projects.add(project);
-			} else if (resource instanceof IFolder) {
-				try {
-					for (final IResource r2 : ((IFolder) resource).members()) {
-						res.addLast(r2);
-					}
-				} catch (CoreException e) {
-					ErrorReporter.logExceptionStackTrace("Error while collecting resources", e);
-				}
-			} else if (resource instanceof IFile) {
-				final IFile file = (IFile) resource;
-				final String extension = file.getFileExtension();
-				if ("ttcn".equals(extension) || "ttcn3".equals(extension)) {
-					final IProject project = file.getProject();
-					List<IFile> filesInProject = files.get(project);
-					if (filesInProject == null) {
-						filesInProject = new ArrayList<IFile>();
-						files.put(project, filesInProject);
-					}
-					filesInProject.add(file);
-				}
 			}
 		}
 	}
@@ -133,37 +98,6 @@ public class CheckCodeSmells extends AbstractHandler {
 							handler = analyzer.analyzeProject(progress.newChild(100), project);
 						}
 						handler.showAll(project);
-					}
-					return Status.OK_STATUS;
-				}
-			}.quickSchedule(project);
-		}
-	}
-
-	/**
-	 * @param files the files to be checked outside of projects.
-	 * @param onTheFlyEnabled whether on-the-fly analysis is enabled or not
-	 * @param analyzer the analyzer to be used to analyze the projects
-	 */
-	private void checkSeparateFiles(final Map<IProject, List<IFile>> files, final boolean onTheFlyEnabled, final Analyzer analyzer) {
-		for (final IProject project : files.keySet()) {
-			new ProjectAnalyzerJob("Check some files in " + project.getName() + " for code smells") {
-				@Override
-				public IStatus doPostWork(final IProgressMonitor monitor) {
-					final SubMonitor progress = SubMonitor.convert(monitor, files.get(project).size());
-					if (!onTheFlyEnabled) {
-						final ProjectSourceParser projectSourceParser = GlobalParser.getProjectSourceParser(project);
-						for (final IFile file : files.get(project)) {
-							final Module module = projectSourceParser.containedModule(file);
-							if (module != null) {
-								progress.subTask("Analyzing module " + module.getName());
-								MarkerHandler handler;
-								synchronized (project) {
-									handler = analyzer.analyzeModule(progress.newChild(1), module);
-								}
-								handler.showAll(project);
-							}
-						}
 					}
 					return Status.OK_STATUS;
 				}
