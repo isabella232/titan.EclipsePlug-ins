6880b05b9b435b475541823eb3c33d8cd4b277f9
zlovarp <arpad.lovassy@semcon.com>
2020-08-14 10:36:06 +0200

    JSON descriptors changed because of deprecated constructor
    Signed-off-by: zlovarp <arpad.lovassy@semcon.com>

--- org.eclipse.titan.runtime/src/org/eclipse/titan/runtime/core/TitanBitString.java
+++ org.eclipse.titan.runtime/src/org/eclipse/titan/runtime/core/TitanBitString.java
@@ -46,7 +46,7 @@ public class TitanBitString extends Base_Type {
 	private static final ASN_Tag TitanBitString_tag_[] = new ASN_Tag[] {new ASN_Tag(ASN_TagClass.ASN_TAG_UNIV, 3)};
 	public static final ASN_BERdescriptor TitanBitString_Ber_ = new ASN_BERdescriptor(1, TitanBitString_tag_);
 	public static final TTCN_RAWdescriptor TitanBitString_raw_ = new TTCN_RAWdescriptor(0, raw_sign_t.SG_NO, raw_order_t.ORDER_LSB, raw_order_t.ORDER_LSB, raw_order_t.ORDER_LSB, raw_order_t.ORDER_LSB, ext_bit_t.EXT_BIT_NO, raw_order_t.ORDER_LSB, raw_order_t.ORDER_LSB, top_bit_order_t.TOP_BIT_INHERITED, 0, 0, 0, 8, 0, null, -1, CharCoding.UNKNOWN, null, false);
-	public static final TTCN_JSONdescriptor TitanBitString_json_ = new TTCN_JSONdescriptor(false, null, false, null, false, false, false, 0, null, false, json_string_escaping.ESCAPE_AS_SHORT);
+	public static final TTCN_JSONdescriptor TitanBitString_json_ = new TTCN_JSONdescriptor(false, null, false, null, null, false, false, false, 0, null, false, json_string_escaping.ESCAPE_AS_SHORT);
 	public static final TTCN_Typedescriptor TitanBitString_descr_ = new TTCN_Typedescriptor("BIT STRING", TitanBitString_Ber_, TitanBitString_raw_, TitanBitString_json_, null);
 
 	/**
--- org.eclipse.titan.runtime/src/org/eclipse/titan/runtime/core/TitanBoolean.java
+++ org.eclipse.titan.runtime/src/org/eclipse/titan/runtime/core/TitanBoolean.java
@@ -45,7 +45,7 @@ public class TitanBoolean extends Base_Type {
 	private static final ASN_Tag TitanBoolean_tag_[] = new ASN_Tag[] {new ASN_Tag(ASN_TagClass.ASN_TAG_UNIV, 1)};
 	public static final ASN_BERdescriptor TitanBoolean_Ber_ = new ASN_BERdescriptor(1, TitanBoolean_tag_);
 	public static final TTCN_RAWdescriptor TitanBoolean_raw_ = new TTCN_RAWdescriptor(1, raw_sign_t.SG_NO, raw_order_t.ORDER_LSB, raw_order_t.ORDER_LSB, raw_order_t.ORDER_LSB, raw_order_t.ORDER_LSB, ext_bit_t.EXT_BIT_NO, raw_order_t.ORDER_LSB, raw_order_t.ORDER_LSB, top_bit_order_t.TOP_BIT_INHERITED, 0, 0, 0, 8, 0, null, -1, CharCoding.UNKNOWN, null, false);
-	public static final TTCN_JSONdescriptor TitanBoolean_json_ = new TTCN_JSONdescriptor(false, null, false, null, false, false, false, 0, null, false, json_string_escaping.ESCAPE_AS_SHORT);
+	public static final TTCN_JSONdescriptor TitanBoolean_json_ = new TTCN_JSONdescriptor(false, null, false, null, null, false, false, false, 0, null, false, json_string_escaping.ESCAPE_AS_SHORT);
 	public static final TTCN_Typedescriptor TitanBoolean_descr_ = new TTCN_Typedescriptor("BOOLEAN", TitanBoolean_Ber_, TitanBoolean_raw_, TitanBoolean_json_, null);
 
 	/**
