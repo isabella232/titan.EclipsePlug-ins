e4a0c115cc57348568f6bb44f60196ec2f7e9b7d
balaskoa <Jeno.Balasko@ericsson.com>
2020-10-26 16:05:35 +0100

    map_params test is already working (with 3 DTE :( )
    Signed-off-by: balaskoa <Jeno.Balasko@ericsson.com>
    Change-Id: Ic2cafc060ee43230416bf78d26c9b54bf26b4164

---
+++ Regression_Test_java/user_provided/org/eclipse/titan/Regression_Test_java/user_provided/PT.java
@@ -0,0 +1,18 @@
+package org.eclipse.titan.Regression_Test_java.user_provided;
+
+import org.eclipse.titan.Regression_Test_java.generated.map__param__common.PT_BASE;
+import org.eclipse.titan.runtime.core.TitanCharString;
+
+public class PT extends PT_BASE {
+
+	public PT(String port_name) {
+		super(port_name);
+	}
+
+	@Override
+	protected void outgoing_send(TitanCharString send_par) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
