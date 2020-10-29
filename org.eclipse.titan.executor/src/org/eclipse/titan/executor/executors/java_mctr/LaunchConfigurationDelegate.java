394b69ec3bed1ab0ddd8e804357fbd76562c376d
Kristof Szabados <Kristof.Szabados@ericsson.com>
2020-10-29 07:23:17 +0100

    there is no need to fix the nativ Java MainController to a version of the installed C side mainController version.
    Signed-off-by: Kristof Szabados <Kristof.Szabados@ericsson.com>

--- org.eclipse.titan.executor/src/org/eclipse/titan/executor/executors/java_mctr/LaunchConfigurationDelegate.java
+++ org.eclipse.titan.executor/src/org/eclipse/titan/executor/executors/java_mctr/LaunchConfigurationDelegate.java
@@ -11,7 +11,6 @@ import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.debug.core.ILaunch;
 import org.eclipse.debug.core.ILaunchConfiguration;
-import org.eclipse.titan.common.product.ProductIdentity;
 import org.eclipse.titan.executor.executors.TitanLaunchConfigurationDelegate;
 
 /**
@@ -20,37 +19,8 @@ import org.eclipse.titan.executor.executors.TitanLaunchConfigurationDelegate;
  */
 public final class LaunchConfigurationDelegate extends TitanLaunchConfigurationDelegate {
 
-	/** { ttcn3_major, ttcn3_minor, ttcn3_patchlevel, ttcn3_buildnumber } */
-	private final ProductIdentity versionLow = ProductIdentity.getProductIdentity(ProductIdentity.TITAN_PRODUCT_NUMBER, 7, 1, 1, 0);
-	private final ProductIdentity versionHigh = ProductIdentity.getProductIdentity(ProductIdentity.TITAN_PRODUCT_NUMBER, 7, 1, 1, 0);
-
 	@Override
 	public void launch(final ILaunchConfiguration arg0, final String arg1, final ILaunch arg2, final IProgressMonitor arg3) throws CoreException {
-		//FIXME check if we will need to check the version of the MainController or not
-//		final Exception exception = JNIMiddleWare.getException();
-//		if (null != exception) {
-//			final Status status = new Status(IStatus.ERROR, Activator.PLUGIN_ID, IStatus.OK, exception.getMessage(), exception);
-//			throw new CoreException(status);
-//		}
-//
-//		final long version = JNIMiddleWare.getSharedLibraryVersion();
-//		final ProductIdentity currentVersion = ProductIdentity.getProductIdentity(ProductIdentity.TITAN_PRODUCT_NUMBER, version);
-//
-//		if (versionLow.compareTo(currentVersion) > 0 || versionHigh.compareTo(currentVersion) < 0) {
-//			Status status;
-//			if (versionLow.equals(versionHigh)) {
-//				status = new Status(IStatus.ERROR, Activator.PLUGIN_ID, IStatus.OK, "Bad version of JNI dynamic library detected.\n"
-//						+ "Supported version: "  + versionHigh + "\n"
-//						+ "Current version: " + currentVersion, null);
-//			} else {
-//				status = new Status(IStatus.ERROR, Activator.PLUGIN_ID, IStatus.OK, "Bad version of JNI dynamic library detected.\n"
-//						+ "Supported versions: " + versionLow + " - " + versionHigh + "\n"
-//						+ "Current version: " + currentVersion,
-//						null);
-//			}
-//			throw new CoreException(status);
-//		}
-
 		showExecutionPerspective();
 		final NativeJavaExecutor executor = new NativeJavaExecutor(arg0);
 		executor.startSession(arg2);
